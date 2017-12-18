be041d75347e5b04674f59f7c72c401fd1d1e993
Pete Jemian <prjemian@gmail.com>
2017-12-18 17:28:47 -0600

    fixes #13 (and code cleanup)

--- APS_BlueSky_tools/zmq_pair.py
+++ APS_BlueSky_tools/zmq_pair.py
@@ -69,69 +69,15 @@ class ZMQ_Pair(object):
 _cache_ = {}
 
 
-def server_example():
-    """
-    ::
-    
-        from zmq_pair import server_example
-        server_example()
-    
-    """
-    from zmq_pair import ZMQ_Pair
-    import socket
-
-    listener = ZMQ_Pair()
-    print("0MQ server Listening now: {}".format(str(listener)))
-    print("  host: {}".format(listener.host))
-    print("  port: {}".format(listener.port))
-    print("  addr: {}".format(listener.addr))
-    serverhost = socket.gethostname() or 'localhost' 
-    print("  serverhost: {}".format(serverhost))
-    print("#"*40)
-    while True:
-        msg = listener.receive()
-        #print(msg.decode())
-        if len(msg) < 12:
-            print(type(msg), msg.decode())
-        else:
-            print(type(msg), "length={}".format(len(msg)))
-        if str(msg) == str(listener.eot_signal_text):
-            break
-    print("\n 0MQ server stopped listening")
-
-
-def client_example(filename, host=None):
-    """
-    ::
-
-        from zmq_pair import client_example
-        client_example("zmq_pair.py")   # on localhost
-        # or
-        client_example("zmq_pair.py", "10.0.2.2") # on VM
-
-    """
-    from zmq_pair import ZMQ_Pair
-
-    talker = ZMQ_Pair(host or "localhost")
-    print("Starting 0MQ client: {}".format(str(talker)))
-    print("  host: {}".format(talker.host))
-    print("  port: {}".format(talker.port))
-    print("  addr: {}".format(talker.addr))
-    print("#"*40)
-    # send this file over 0mq as an example
-    with open(filename, 'r') as f:
-        for line in f:
-            talker.send_string(line.rstrip("\n"))
-    talker.send_string(talker.eot_signal_text.decode())
-    print("\nEnding 0MQ client")
-
-
 def mona_zmq_sender(
         sender, key, 
         document, 
         detector, 
         signal_name=None,
-        rotation_name=None):
+        rotation_name=None,
+        max_motor_still_time=0.1,
+        **kwds
+        ):
     '''
     send documents from BlueSky events for the MONA project via a ZMQ pair
     
@@ -139,6 +85,30 @@ def mona_zmq_sender(
     
     This code is called from a BlueSky callback
     
+    PARAMETERS
+    
+    sender : obj
+        instance of ZMQ_Pair()
+    key : str
+        name of BlueSky stream document
+    document : dict
+        BlueSky JSON document with stream information
+    detector : obj
+        ophyd area detector image Device with image data
+    signal_name : str
+        name of ophyd Signal that notifies a new image is available
+    rotation_name : str
+        name of ophyd Motor that is the rotation axis
+    max_motor_still_time : float
+        maximum timestamp difference between (image-motor)
+        when images will be sent over the ZMQ connection.
+        This suppresses extra images at the end of the scan
+        as well as multiple images while the motor waits
+        at one location with no updates.
+        
+        .. note:: Likely there is a better way to solve #13 than this.
+
+
     .. TODO: EXAMPLE::
     
         --update-- --needed--
@@ -187,12 +157,13 @@ def mona_zmq_sender(
         image_number = document["data"].get(signal_name)
         rotation = _cache_.get("rotation")
         if None in (image_number, rotation):
+            # No image?, No angle? nothing to do.
             return
 
         rotation_time = _cache_["rotation_time"]
         image_time = document["timestamps"].get(signal_name)
-        MOTOR_STILL_TOO_LONG_s = 0.1
-        if (image_time - rotation_time) > MOTOR_STILL_TOO_LONG_s:
+        if (image_time - rotation_time) > max_motor_still_time:
+            # motor has not updated in a while, no image
             return
 
         # pump out the image
@@ -248,8 +219,8 @@ def mona_zmq_receiver(*args, **kwds):
             image = numpy.frombuffer(image, dtype=dtype).reshape(shape)
             print("image", 
                   md["image_number"],
-                  md["rotation"], 
-                  md["image_timestamp"]-md["rotation_timestamp"])
+                  "%.6f" % md["rotation"], 
+                  "%.4f" % (md["image_timestamp"]-md["rotation_timestamp"]))
         elif key == "bulk_events":
             #print(key, md["document"])
             pass
@@ -264,173 +235,5 @@ def mona_zmq_receiver(*args, **kwds):
     print("Connection ended")
 
 
-def __PREVIOUS__mona_zmq_receiver(filename):
-    """
-    receive documents from BlueSky events for the MONA project
-    
-    This is the ZMQ server end of the pipe
-    
-    create a ZMQ pair server before starting BlueSky
-    
-    This code runs on the data processing stream computer *before* BlueSky starts.
-    The BlueSky callback will attempt to connect with this server as a client
-    using the ZMQ PAIR protocol.  BlueSky will then emit documents,
-    some of which contain images to be reconstructed, until BlueSky sends an
-    "end" document signalling this server that no more events will be sent.
-    
-    This example code writes the collected images into a NeXus HDF5 data file.
-    Drop that for production, it's just a demo and diagnostic.
-    
-    EXAMPLE::
-    
-        mona_zmq_receiver("/tmp/mona_receiver.hdf5")
-    
-    """
-    from zmq_pair import ZMQ_Pair
-    import datetime
-    import h5py
-    import json
-    import socket
-    
-    hdf5_data_compression = "gzip"
-    # hdf5_data_compression = "lzf"
-    
-    def process_message():
-        """
-        get messages from 0MQ and convert them back to original form
-        
-        Messages are in the form of (key, buffer) pairs
-        where key is one of (start descriptor event stop bulk_events image)
-        and buffer is json for all except image, which is binary data.
-        """
-        # TODO: Can ZMQ tell us if the connection has been dropped?
-        msg = listener.receive()
-        if str(msg) == str(listener.eot_signal_text):
-            # TODO: propagate end() message to clients
-            return ()
-        key = msg.decode()
-        if key in ("start", "descriptor", "event", "stop", "bulk_events"):
-            document = listener.receive().decode()
-            try:
-                return key, json.loads(document)
-            except Exception as msg:
-                print(msg)
-                return ()
-        elif key == "image":
-            s = listener.receive().decode().rstrip(')').lstrip('(').split(',')
-            shape = tuple(map(int, s))
-            dtype = listener.receive().decode()
-            image_number = int(listener.receive().decode())
-            image_timestamp = float(listener.receive().decode())
-            rotation = float(listener.receive().decode())
-            rotation_timestamp = float(listener.receive().decode())
-
-            # see: https://stackoverflow.com/questions/28995937/convert-python-byte-string-to-numpy-int#28996024
-            image = listener.receive()
-            image = numpy.fromstring(image, dtype=dtype).reshape(shape)
-            return key, {
-                "image": image,
-                "image_number": image_number,
-                "image_timestamp": image_timestamp,
-                "rotation": rotation,
-                "rotation_timestamp": rotation_timestamp}
-
-    listener = ZMQ_Pair()
-    print("0MQ server Listening now: {}".format(str(listener)))
-    print("  host: {}".format(listener.host))
-    print("  port: {}".format(listener.port))
-    print("  addr: {}".format(listener.addr))
-    serverhost = socket.gethostname() or 'localhost' 
-    print("  serverhost: {}".format(serverhost))
-    print("#"*40)
-
-    nexus = h5py.File(filename, "w")
-    nexus.attrs["filename"] = filename
-    nexus.attrs["file_time"] = str(datetime.datetime.now())
-    nexus.attrs["creator"] = "BlueSky ZMQ Callback"
-    nexus.attrs["H5PY_VERSION"] = h5py.__version__
-    nexus.close()
-    
-    doc_stream = []
-    
-    while True:
-        try:
-            results = process_message()
-        except Exception as msg:
-            print(msg)
-            continue
-        if len(results) == 0:
-            # TODO: propagate end() message to clients
-            break
-        key, document = results
-        if key == "start":
-            uid = document["uid"]
-            entry_name = "entry_" + uid[:8]
-            local_image_number = 0
-
-            nexus = h5py.File(filename, "a")
-            nexus.attrs["default"] = entry_name
-
-            nxentry = nexus.create_group(entry_name)
-            nxentry.attrs["NX_class"] = "NXentry"
-            nxentry.attrs["default"] = "data"
-            ds = nxentry.create_dataset("experiment_identifier", data=uid)
-            ds.attrs["meaning"] = "UUID"
-
-            docs_group = nxentry.require_group("BlueSky_document_stream")
-            docs_group.attrs["NX_class"] = "NXnote"
-
-            item_name = key+"_" + document["uid"][:8]
-            doc_stream.append(item_name)
-            nxnote = docs_group.create_group(item_name)
-            nxnote.attrs["NX_class"] = "NXnote"
-            nxnote.create_dataset("json", data=json.dumps(document))
-
-            nxdata = nxentry.create_group("data")
-            nxdata.attrs["NX_class"] = "NXdata"
-        elif key == "stop":
-            item_name = key+"_" + document["uid"][:8]
-            doc_stream.append(item_name)
-            nxnote = docs_group.create_group(item_name)
-            nxnote.attrs["NX_class"] = "NXnote"
-            nxnote.create_dataset("json", data=json.dumps(document))
-            docs_group.attrs["document_sequence"] = "\n".join(doc_stream)
-            nexus.close()
-            doc_stream = []
-        elif key in ("event", "descriptor"):
-            item_name = key+"_" + document["uid"][:8]
-            doc_stream.append(item_name)
-            nxnote = docs_group.create_group(item_name)
-            nxnote.attrs["NX_class"] = "NXnote"
-            nxnote.create_dataset("json", data=json.dumps(document))
-        elif key == "image":
-            data_name = "image_{}".format(local_image_number)
-            try:
-                ds = nxdata.create_dataset(
-                    data_name, 
-                    data = document["image"],
-                    compression = hdf5_data_compression,
-                    )
-            except ValueError as err:
-                print("err", err)
-                print("image size", len(document["image"]))
-                print("compression", hdf5_data_compression)
-                continue
-            ds.attrs["units"] = "counts"
-            ds.attrs["local_image_number"] = local_image_number
-            ds.attrs["AD_image_number"] = document["image_number"]
-            ds.attrs["image_timestamp"] = document["image_timestamp"]
-            ds.attrs["rotation"] = document["rotation"]
-            ds.attrs["rotation_timestamp"] = document["rotation_timestamp"]
-            if local_image_number == 0:
-                nxdata.attrs["signal"] = data_name
-            local_image_number += 1
-        else:
-            pass
-
-    print("\n 0MQ server stopped listening")
-
-
 if __name__ == "__main__":
-    mona_zmq_receiver("/tmp/mona_receiver.hdf5")
-#     server_example()
+    mona_zmq_receiver()
