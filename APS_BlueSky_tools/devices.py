e2dd9131d4aa145d822bd3dedc87f78e54ca90b3
jemian <prjemian@gmail.com>
2018-01-31 17:22:00 -0600

    couple common motor variations

--- APS_BlueSky_tools/devices.py
+++ APS_BlueSky_tools/devices.py
@@ -12,10 +12,32 @@
     ~swait_setup_lorentzian
     ~swait_setup_incrementer
     ~userCalcsDevice
+    ~EpicsMotorWithDial
+    ~EpicsMotorWithServo
 
 """
 
-# Copyright (c) 2017-, UChicago Argonne, LLC.  See LICENSE file.
+# Copyright (c) 2017-2018, UChicago Argonne, LLC.  See LICENSE file.
 
 
 from .synApps_ophyd import *
+from ophyd import Component, EpicsMotor
+
+
+class EpicsMotorWithDial(EpicsMotor):
+    """
+    add motor record's dial coordinates to EpicsMotor
+    
+    USAGE::
+    
+        m1 = EpicsMotorWithDial('xxx:m1', name='m1')
+    
+    """
+    dial = Component(EpicsSignal, ".DRBV", write_pv=".DVAL")
+
+
+class EpicsMotorWithServo(EpicsMotor):
+    """extend basic motor support to enable/disable the servo loop controls"""
+    
+    # values: "Enable" or "Disable"
+    servo = Component(EpicsSignal, ".CNEN", string=True)
