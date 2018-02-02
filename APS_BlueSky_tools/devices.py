943bbe16a4a492456d76b96ee61ba0f53995a481
Pete Jemian <prjemian@gmail.com>
2018-02-02 12:46:01 -0600

    adds APS PSS Shutter device

--- APS_BlueSky_tools/devices.py
+++ APS_BlueSky_tools/devices.py
@@ -4,6 +4,10 @@
 
 .. autosummary::
    
+    ~ApsPssShutter
+    ~EpicsMotorWithDial
+    ~EpicsMotorWithServo
+    ~EpicsMotorShutter
     ~sscanRecord  
     ~sscanDevice
     ~swaitRecord
@@ -12,19 +16,108 @@
     ~swait_setup_lorentzian
     ~swait_setup_incrementer
     ~userCalcsDevice
-    ~EpicsMotorWithDial
-    ~EpicsMotorWithServo
-    ~EpicsMotorShutter
 
 """
 
 # Copyright (c) 2017-2018, UChicago Argonne, LLC.  See LICENSE file.
 
 
+import threading
+import time
 from .synApps_ophyd import *
 from ophyd import Component, Device, DeviceStatus
-from ophyd import EpicsMotor, EpicsSignal
+from ophyd import Signal, EpicsMotor, EpicsSignal
 from bluesky.plan_stubs import mv, mvr, abs_set, wait
+
+
+class ApsPssShutter(Device):
+    """
+    APS PSS shutter
+    
+    * APS PSS shutters have separate bit PVs for open and close
+    * set either bit, the shutter moves, and the bit resets a short time later
+    * no indication that the shutter has actually moved from the bits
+    
+    USAGE:
+    
+        shutter_a = ApsPssShutter("2bma:A_shutter", name="shutter")
+        
+        shutter_a.open()
+        shutter_a.close()
+        
+        shutter_a.set("open")
+        shutter_a.set("close")
+        
+    When using the shutter in a plan, be sure to use `yield from`.
+
+        def in_a_plan(shutter):
+            yield from abs_set(shutter, "open", wait=True)
+            # do something
+            yield from abs_set(shutter, "close", wait=True)
+        
+        RE(in_a_plan(shutter_a))
+        
+    The strings accepted by `set()` are defined in two lists:
+    `valid_open_values` and `valid_close_values`.  These lists
+    are treated (internally to `set()`) as lower case strings.
+    
+    Example, add "o" & "x" as aliases for "open" & "close":
+    
+        shutter_a.valid_open_values.append("o")
+        shutter_a.valid_close_values.append("x")
+        shutter_a.set("o")
+        shutter_a.set("x")
+    """
+    open_bit = Component(EpicsSignal, ":open")
+    close_bit = Component(EpicsSignal, ":close")
+    delay_s = 1.2
+    valid_open_values = ["open",]   # lower-case strings ONLY
+    valid_close_values = ["close",]
+    busy = Signal(value=False, name="busy")
+    
+    def open(self):
+        """request shutter to open, interactive use"""
+        self.open_bit.put(1)
+    
+    def close(self):
+        """request shutter to close, interactive use"""
+        self.close_bit.put(1)
+    
+    def set(self, value, **kwargs):
+        """request the shutter to open or close, BlueSky plan use"""
+        # ensure numerical additions to lists are now strings
+        def input_filter(v):
+            return str(v).lower()
+        self.valid_open_values = list(map(input_filter, self.valid_open_values))
+        self.valid_close_values = list(map(input_filter, self.valid_close_values))
+        
+        if self.busy.value:
+            raise RuntimeError("shutter is operating")
+
+        acceptables = self.valid_open_values + self.valid_close_values
+        if input_filter(value) not in acceptables:
+            msg = "value should be one of " + " | ".join(acceptables)
+            msg += " : received " + str(value)
+            raise ValueError(msg)
+        
+        status = DeviceStatus(self)
+        
+        def move_shutter():
+            if input_filter(value) in self.valid_open_values:
+                self.open()     # no need to yield inside a thread
+            elif input_filter(value) in self.valid_close_values:
+                self.close()
+        
+        def run_and_delay():
+            self.busy.put(True)
+            move_shutter()
+            # sleep, since we don't *know* when the shutter has moved
+            time.sleep(self.delay_s)
+            self.busy.put(False)
+            status._finished(success=True)
+        
+        threading.Thread(target=run_and_delay, daemon=True).start()
+        return status
 
 
 class EpicsMotorWithDial(EpicsMotor):
@@ -48,7 +141,7 @@ class EpicsMotorWithServo(EpicsMotor):
 
 class EpicsMotorShutter(Device):
     """
-    a shutter, implemented with an EPICS motor
+    a shutter, implemented with an EPICS motor moved between two positions
     
     USAGE::
         tomo_shutter = EpicsMotorShutter("2bma:m23", name="tomo_shutter")
