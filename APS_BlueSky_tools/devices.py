dfc41c249d84560b8ab5ff41acb4fa0096d4b6ea
Pete Jemian <prjemian@gmail.com>
2018-08-03 14:32:32 -0500

    fixes #44

--- APS_BlueSky_tools/devices.py
+++ APS_BlueSky_tools/devices.py
@@ -10,6 +10,7 @@
     ~ApsPssShutter
     ~AxisTunerException
     ~AxisTunerMixin
+    ~DualPf4FilterBox
     ~EpicsMotorDialMixin
     ~EpicsMotorServoMixin
     ~EpicsMotorRawMixin
@@ -592,6 +593,34 @@ class EpicsMotorShutter(Device):
             pos, use_complete=True, callback=put_callback)
 
         return status
+
+
+class DualPf4FilterBox(Device):
+    """
+    Dual Xia PF4 filter boxes using support from synApps (using Al, Ti foils)
+    
+    Example::
+    
+        pf4 = DualPf4FilterBox("2bmb:pf4:", name="pf4")
+        pf4_AlTi = DualPf4FilterBox("9idcRIO:pf4:", name="pf4_AlTi")
+    
+    """
+    fPosA = Component(EpicsSignal, "fPosA")
+    fPosB = Component(EpicsSignal, "fPosB")
+    bankA = Component(EpicsSignalRO, "bankA")
+    bankB = Component(EpicsSignalRO, "bankB")
+    bitFlagA = Component(EpicsSignalRO, "bitFlagA")
+    bitFlagB = Component(EpicsSignalRO, "bitFlagB")
+    transmission = Component(EpicsSignalRO, "trans")
+    transmission_a = Component(EpicsSignalRO, "transA")
+    transmission_b = Component(EpicsSignalRO, "transB")
+    inverse_transmission = Component(EpicsSignalRO, "invTrans")
+    thickness_Al_mm = Component(EpicsSignalRO, "filterAl")
+    thickness_Ti_mm = Component(EpicsSignalRO, "filterTi")
+    thickness_glass_mm = Component(EpicsSignalRO, "filterGlass")
+    energy_keV_local = Component(EpicsSignal, "E:local")
+    energy_keV_mono = Component(EpicsSignal, "displayEnergy")
+    mode = Component(EpicsSignal, "useMono", string=True)    
 
 
 # AreaDetector support
