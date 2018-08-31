e8320da4b5eb59ee291d7629eae12983ce1b773c
Pete R. Jemian <prjemian@gmail.com>
2018-08-30 21:02:43 -0500

    #36 w-i-p

--- APS_BlueSky_tools/issue_36_developer.py
+++ APS_BlueSky_tools/issue_36_developer.py
@@ -1,4 +1,5 @@
 
+import numpy as np
 from ophyd.scaler import ScalerCH
 from ophyd import EpicsMotor, Signal, Component, Device
 from APS_BlueSky_tools.devices import use_EPICS_scaler_channels, AxisTunerMixin
@@ -8,6 +9,7 @@ from bluesky.callbacks.best_effort import BestEffortCallback
 from APS_BlueSky_tools.plans import TuneAxis
 
 import datetime
+import time
 from bluesky.callbacks.fitting import PeakStats
 from bluesky import preprocessors as bpp
 from bluesky import plan_stubs as bps
@@ -15,25 +17,38 @@ from bluesky import plan_patterns as bpat
 
 from bluesky.utils import ts_msg_hook
 
+# Import matplotlib and put it in interactive mode.
+import matplotlib.pyplot as plt
+plt.ion()
+
+# Make plots update live while scans run.
+from bluesky.utils import install_qt_kicker
+install_qt_kicker()
+
 
 RE = RunEngine({})
 RE.subscribe(BestEffortCallback())
 
-m1 = EpicsMotor("gov:m1", name="m1")
-scaler = ScalerCH("gov:scaler1", name="scaler")
+m1 = EpicsMotor("xxx:m1", name="m1")
+scaler = ScalerCH("xxx:scaler1", name="scaler")
+
+time.sleep(1)
+
 scaler.match_names()
 use_EPICS_scaler_channels(scaler)
 for k, v in scaler.read().items():
     print(k, v)
 print("scaler.preset_time", scaler.preset_time.value)
-RE.msg_hook = ts_msg_hook
+# RE.msg_hook = ts_msg_hook
 
 
 class TunableEpicsMotor(EpicsMotor, AxisTunerMixin):
     pass
 
+ 
 class MyTuneAxis(TuneAxis):
     
+ 
     # override the default tune() method
 
     def tune(self, width=None, num=None, md=None):
@@ -65,6 +80,7 @@ class MyTuneAxis(TuneAxis):
             raise ValueError(msg)
 
         initial_position = self.axis.position
+        final_position = initial_position       # unless tuned
         start = initial_position - width/2
         finish = initial_position + width/2
         self.tune_ok = False
@@ -90,51 +106,39 @@ class MyTuneAxis(TuneAxis):
             self.stats = []
         self.peaks = PeakStats(x=self.axis.name, y=self.signal_name)
         
-        def tune_scan(detectors, axis, start, finish, num, _md):
-            """run the tune scan"""
-            cycler = bpat.inner_product(num, (axis, start, finish))
-            
-            signal_list = list(detectors)
-            signal_list += [axis,]
-
-            def inner_scan_nd():
-                for step in list(cycler):
-                    pos = list(step.values())[0]    # TODO: simplify?
-                    yield from bps.mv(axis, pos)
-                    yield from bps.trigger_and_read(signal_list)
-
-            return (yield from inner_scan_nd())
-            
         class Results(Device):
             """because bps.read() needs a Device or a Signal)"""
-            x = Component(Signal, name="x")
-            y = Component(Signal, name="y")
-            cen = Component(Signal, name="cen")
-            com = Component(Signal, name="com")
-            fwhm = Component(Signal, name="fwhm")
-            min = Component(Signal, name="min")
-            max = Component(Signal, name="max")
-            crossings = Component(Signal, name="crossings")
-            read_attrs = "x y cen com fwhm min max crossings".split()
+            tune_ok = Component(Signal)
+            initial_position = Component(Signal)
+            final_position = Component(Signal)
+            center = Component(Signal)
+            # - - - - -
+            x = Component(Signal)
+            y = Component(Signal)
+            cen = Component(Signal)
+            com = Component(Signal)
+            fwhm = Component(Signal)
+            min = Component(Signal)
+            max = Component(Signal)
+            crossings = Component(Signal)
+            peakstats_attrs = "x y cen com fwhm min max crossings".split()
+            
+            def report(self):
+                keys = self.peakstats_attrs + "tune_ok center initial_position final_position".split()
+                for key in keys:
+                    print("{} : {}".format(key, getattr(self, key).value))
 
-        def add_results_stream(stream_name="tune_PeakStats"):
-            """add a stream with the tuning results (from PeakStats)"""
-            tune_PeakStats = Results(
-                name="tune_PeakStats",
-                read_attrs=Results.read_attrs,
-                )
-            tune_PeakStats.hints["fields"] = ["tune_PeakStats",]
-            for key in tune_PeakStats.read_attrs:
-                getattr(tune_PeakStats, key).put(getattr(self.peaks, key))
-            yield from bps.create(name=stream_name)
-            yield from bps.read(tune_PeakStats)
-            yield from bps.save()
-        
         @bpp.subs_decorator(self.peaks)
         def _scan():
             yield from bps.open_run()
-            yield from tune_scan(
-                self.signals, self.axis, start, finish, num, _md)
+
+            position_list = np.linspace(start, finish, num)
+            signal_list = list(self.signals)
+            signal_list += [self.axis,]
+            # TODO: need some hints to show the Y axis
+            for pos in position_list:
+                yield from bps.mv(self.axis, pos)
+                yield from bps.trigger_and_read(signal_list)
             
             final_position = initial_position
             if self.peak_detected():
@@ -148,23 +152,38 @@ class MyTuneAxis(TuneAxis):
                 self.center = final_position
 
             # add stream with results
-            yield from add_results_stream()
+            # yield from add_results_stream()
+            stream_name = "PeakStats"
+            results = Results(name=stream_name)
+
+            for key in "tune_ok center".split():
+                getattr(results, key).put(getattr(self, key))
+            results.final_position.put(final_position)
+            results.initial_position.put(initial_position)
+            for key in results.peakstats_attrs:
+                getattr(results, key).put(getattr(self.peaks, key))
+
+            yield from bps.create(name=stream_name)
+            yield from bps.read(results)
+            yield from bps.save()
             
             yield from bps.mv(self.axis, final_position)
             self.stats.append(self.peaks)
             yield from bps.close_run()
+
+            results.report()
     
         return (yield from _scan())
 
 
 def myCallback(key, doc):
-    if key in ("start", "descriptor", "event", "stop"):
+    if key in (" start", " descriptor", " event", " stop"):
         print("-"*20)
         for k, v in doc.items():
             print("\t", key, k, v)
 
 
-m1 = TunableEpicsMotor("gov:m1", name="m1")
+m1 = TunableEpicsMotor("xxx:m1", name="m1")
 m1.tuner = MyTuneAxis([scaler], m1, signal_name="scint")
 m1.tuner.width = 0.02
 m1.tuner.num = 21
