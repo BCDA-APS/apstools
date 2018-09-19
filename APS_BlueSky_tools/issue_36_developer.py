de3c96680a8c7de29182f517347b34a32f55cbdb
Pete Jemian <prjemian@gmail.com>
2018-09-19 16:49:14 -0500

    tune against a swait record peak simulation

--- APS_BlueSky_tools/issue_36_developer.py
+++ APS_BlueSky_tools/issue_36_developer.py
@@ -1,56 +1,24 @@
 
-import socket
-hostname = socket.gethostname()
-
-if hostname in ('otz.aps.anl.gov',):
-    IOC_PREFIX = "gov:"
-elif hostname.endswith('jemian.org'):
-    IOC_PREFIX = "prj:"
-else:
-    IOC_PREFIX = "xxx:"
-
-
 import numpy as np
-from ophyd.scaler import ScalerCH
-from ophyd import EpicsMotor, Signal, Component, Device
-from APS_BlueSky_tools.devices import use_EPICS_scaler_channels, AxisTunerMixin
-from bluesky import RunEngine
-from bluesky.callbacks.best_effort import BestEffortCallback
-
-from APS_BlueSky_tools.plans import TuneAxis
-
-import datetime
+import socket
 import time
-from bluesky.callbacks.fitting import PeakStats
-from bluesky import preprocessors as bpp
-from bluesky import plan_stubs as bps
-from bluesky import plan_patterns as bpat
-
-from bluesky.utils import ts_msg_hook
 
 # Import matplotlib and put it in interactive mode.
 import matplotlib.pyplot as plt
 plt.ion()
 
+from ophyd.scaler import ScalerCH
+from ophyd import EpicsMotor, EpicsSignal
+from bluesky import RunEngine
+from bluesky.callbacks.best_effort import BestEffortCallback
+import bluesky.plans as bp 
 # Make plots update live while scans run.
 from bluesky.utils import install_qt_kicker
 install_qt_kicker()
 
-
-RE = RunEngine({})
-RE.subscribe(BestEffortCallback())
-
-m1 = EpicsMotor(IOC_PREFIX+"m1", name="m1")
-scaler = ScalerCH(IOC_PREFIX+"scaler1", name="scaler")
-
-time.sleep(1)
-
-scaler.match_names()
-use_EPICS_scaler_channels(scaler)
-for k, v in scaler.read().items():
-    print(k, v)
-print("scaler.preset_time", scaler.preset_time.value)
-# RE.msg_hook = ts_msg_hook
+from APS_BlueSky_tools.devices import use_EPICS_scaler_channels, AxisTunerMixin
+from APS_BlueSky_tools.plans import TuneAxis
+from APS_BlueSky_tools.synApps_ophyd import userCalcsDevice, swait_setup_lorentzian
 
 
 class TunableEpicsMotor(EpicsMotor, AxisTunerMixin):
@@ -68,9 +36,60 @@ def myCallback(key, doc):
             print("\t", key, k, v)
 
 
+def setRandomPeak(calc, motor):
+    swait_setup_lorentzian(
+        calc, 
+        motor, 
+        center = -1.5 + 0.5*np.random.uniform(), 
+        noise  = 0.2  + 0.2*np.random.uniform(), 
+        width  = 0.001 + 0.05*np.random.uniform(), 
+        scale  = 1e5,
+        )
+
+
+hostname = socket.gethostname()
+
+HOST_PV_PREFIX_DICT = {
+    'otz.aps.anl.gov': "gov:",
+    'mint-vm': "prj:",
+    'poof': "prj:",
+    'enoki': "prj:",
+    }
+IOC_PREFIX = HOST_PV_PREFIX_DICT.get(hostname, "xxx:")
+
+
+RE = RunEngine({})
+RE.subscribe(BestEffortCallback())
+
+m1 = EpicsMotor(IOC_PREFIX+"m1", name="m1")
+scaler = ScalerCH(IOC_PREFIX+"scaler1", name="scaler")
+calcs = userCalcsDevice(IOC_PREFIX, name="calcs")
+
+time.sleep(1)
+scaler.channels.chan01.chname.put("clock")
+scaler.channels.chan02.chname.put("I0")
+scaler.channels.chan03.chname.put("scint")
+scaler.channels.chan04.chname.put("Jake")
+scaler.channels.chan05.chname.put("")
+
+scaler.match_names()
+use_EPICS_scaler_channels(scaler)
+for k, v in scaler.read().items():
+    print(k, v)
+print("scaler.preset_time", scaler.preset_time.value)
+
+# change soft motor resolution from 200 steps/rev to 8000 (steps of 0.00025)
+_srev = EpicsSignal(m1.prefix+".SREV", name="_srev")
+_srev.put(8000)
+
 m1 = TunableEpicsMotor(IOC_PREFIX+"m1", name="m1")
-m1.tuner = MyTuneAxis([scaler], m1, signal_name="scint")
-m1.tuner.width = 0.02
+noisy_calc = calcs.calc1
+setRandomPeak(noisy_calc, m1)
+noisy = EpicsSignal(noisy_calc.prefix, name="noisy")
+
+m1.tuner = TuneAxis([noisy], m1, signal_name=noisy.name)
+# m1.tuner = MyTuneAxis([det], m1, signal_name="det")
+m1.tuner.width = 5
 m1.tuner.num = 21
 
 
