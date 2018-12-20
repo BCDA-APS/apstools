e431a674e1ee325932d59d7b5136604a627cfb62
prjemian <prjemian@gmail.com>
2018-12-20 15:22:09 -0600

    #63 RTD Devices superclasses

--- APS_BlueSky_tools/devices.py
+++ APS_BlueSky_tools/devices.py
@@ -67,6 +67,7 @@ from . import plans as APS_plans
 import ophyd
 from ophyd import Component, Device, DeviceStatus, FormattedComponent
 from ophyd import Signal, EpicsMotor, EpicsSignal, EpicsSignalRO
+from ophyd.ophydobj import OphydObject
 from ophyd.scaler import EpicsScaler, ScalerCH
 from bluesky.plan_stubs import mv, mvr, abs_set, wait
 
@@ -570,7 +571,7 @@ class AxisTunerException(ValueError):
     pass
 
 
-class AxisTunerMixin(object):
+class AxisTunerMixin(OphydObject):
     """
     Mixin class to provide tuning capabilities for an axis
     
