b1c0af8ed4ea4b2fa79a570db74fdbdd3450f0ac
Pete Jemian <prjemian@gmail.com>
2019-02-13 13:18:01 -0600

    #86 refactor new base class

--- apstools/devices.py
+++ apstools/devices.py
@@ -242,13 +242,6 @@ class ShutterBase(Device):
     value : str
         any from ``self.choices`` (typically "open" or "close")
 
-    signal : EpicsSignal or Signal
-        (override in subclass)
-        The ``signal`` is the comunication to the hardware.
-        In a subclass, the hardware may have more than
-        one communication channel to use.  See the
-        ``ApsPssShutter`` as an example.
-
     valid_open_values : [str]
         A list of lower-case text values that are acceptable 
         for use with the ``set()`` command to open the shutter.
@@ -258,12 +251,12 @@ class ShutterBase(Device):
         for use with the ``set()`` command to close the shutter.
 
     open_value : number
-        The actual value to send to ``signal`` to open the shutter.
+        The actual value to send to open ``signal`` to open the shutter.
         (default = 1)
 
     close_value : number
-        The actual value to send to ``signal`` to close the shutter.
-        (default = 1)
+        The actual value to send to close ``signal`` to close the shutter.
+        (default = 0)
 
     delay_s : float
         time to wait (s) after move is complete, 
@@ -277,12 +270,174 @@ class ShutterBase(Device):
         (constant) Text reported by ``state`` when not open or closed.
         cannot move to this position
         (default = "unknown")
+    """
+
+    valid_open_values = ["open", "opened",]   # lower-case strings ONLY
+    valid_close_values = ["close", "closed",]
+    open_value = 1      # value of "open"
+    close_value = 0     # value of "close"
+    delay_s = 0.0       # time to wait (s) after move is complete
+    busy = Component(Signal, value=False)
+    unknown_state = "unknown"       # cannot move to this position
+
+    # - - - - likely to override these methods in subclass - - - -
+
+    def open(self):
+        """BLOCKING: request shutter to open, called by set()"""
+        raise NotImplementedError("must implement in subclass")
+        """ example code
+        if not self.isOpen:
+            self.signal.put(self.open_value)
+            if self.delay_s > 0:
+                time.sleep(self.delay_s)    # blocking call OK here
+        """
+
+    def close(self):
+        """BLOCKING: request shutter to close, called by set()"""
+        raise NotImplementedError("must implement in subclass")
+        """ example code
+        if not self.isClosed:
+            self.signal.put(self.close_value)
+            if self.delay_s > 0:
+                time.sleep(self.delay_s)    # blocking call OK here
+        """
+
+    @property
+    def state(self):
+        """returns 'open', 'close', or 'unknown'"""
+        raise NotImplementedError("must implement in subclass")
+        """ example code
+        if self.signal.value == self.open_value:
+            result = self.valid_open_values[0]
+        elif self.signal.value == self.close_value:
+            result = self.valid_close_values[0]
+        else:
+            result = self.unknown_state
+        return result
+        """
+    
+    # - - - - - - possible to override in subclass - - - - - -
+    
+    def __init__(self, prefix, *args, **kwargs):
+        super().__init__(prefix, *args, **kwargs)
+        self.valid_open_values = list(map(self.lowerCaseString, self.valid_open_values))
+        self.valid_close_values = list(map(self.lowerCaseString, self.valid_close_values))
+    
+    @property
+    def isOpen(self):
+        """is the shutter open?"""
+        return str(self.state) == self.valid_open_values[0]
+    
+    @property
+    def isClosed(self):
+        """is the shutter closed?"""
+        return str(self.state) == self.valid_close_values[0]
+
+    def inPosition(self, target):
+        """is the shutter at the target position?"""
+        self.validTarget(target)
+        __value__ = self.lowerCaseString(target)
+        if __value__ in self.valid_open_values and self.isOpen:
+            return True
+        elif __value__ in self.valid_close_values and self.isClosed:
+            return True
+        return False
+
+    def set(self, value, **kwargs):
+        """
+        plan: request the shutter to open or close
+
+        PARAMETERS
+        
+        value : str
+            any from ``self.choices`` (typically "open" or "close")
+        
+        kwargs : dict
+            ignored at this time
+
+        """
+        if self.busy.value:
+            raise RuntimeError("shutter is operating")
+        
+        __value__ = self.lowerCaseString(value)
+        self.validTarget(__value__)
+
+        status = DeviceStatus(self)
+        
+        if self.inPosition(__value__):
+            # no need to move, cut straight to the end
+            status._finished(success=True)
+        else:
+            def move_it():
+                # runs in a thread, no need to "yield from"
+                self.busy.put(True)
+                if __value__ in self.valid_open_values:
+                    self.open()
+                elif __value__ in self.valid_close_values:
+                    self.close()
+                self.busy.put(False)
+                status._finished(success=True)
+            # get it moving
+            threading.Thread(target=move_it, daemon=True).start()
+        return status
+    
+    # - - - - - - not likely to override in subclass - - - - - -
+
+    def addCloseValue(self, text):
+        """a synonym to close the shutter, use with set()"""
+        self.valid_close_values.append(self.lowerCaseString(text))
+        return self.choices     # return the list of acceptable values
+    
+    def addOpenValue(self, text):
+        """a synonym to open the shutter, use with set()"""
+        self.valid_open_values.append(self.lowerCaseString(text))
+        return self.choices     # return the list of acceptable values
+
+    @property
+    def choices(self):
+        """return list of acceptable choices for set()"""
+        return self.valid_open_values + self.valid_close_values
+    
+    def lowerCaseString(self, value):
+        """ensure any given value is a lower-case string"""
+        return str(value).lower()
+
+    def validTarget(self, target, should_raise=True):
+        """
+        return whether (or not) target value is acceptable for self.set()
+        
+        raise ValueError if not acceptable (default)
+        """
+        acceptable_values = self.choices
+        ok = self.lowerCaseString(target) in acceptable_values
+        if not ok and should_raise:
+            msg = "received " + str(target)
+            msg += " : should be only one of "
+            msg += " | ".join(acceptable_values)
+            raise ValueError(msg)
+        return ok
+
+
+class OneSignalShutter(ShutterBase):
+    """
+    shutter Device using one Signal for open and close
+    
+    PARAMETERS
+
+    signal : EpicsSignal or Signal
+        (override in subclass)
+        The ``signal`` is the comunication to the hardware.
+        In a subclass, the hardware may have more than
+        one communication channel to use.  See the
+        ``ApsPssShutter`` as an example.
+    
+    See ``ShutterBase`` for more parameters.
 
     EXAMPLE
 
-    Create a shutter from an EPICS PV:
+    Create a simulated shutter:
 
-        shutter = ShutterBase("", name="shutter")
+        shutter = OneSignalShutter("", name="shutter")
 
     open the shutter (interactively):
 
@@ -329,20 +484,7 @@ class ShutterBase(Device):
         shutter.set("x")
     """
 
-    signal = Component(Signal, value=0)        # override in subclass
-
-    valid_open_values = ["open", "opened",]   # lower-case strings ONLY
-    valid_close_values = ["close", "closed",]
-    open_value = 1      # value of "open"
-    close_value = 0     # value of "close"
-    delay_s = 0.0       # time to wait (s) after move is complete
-    busy = Component(Signal, value=False)
-    unknown_state = "unknown"       # cannot move to this position
-
-    def __init__(self, prefix, *args, **kwargs):
-        super().__init__(prefix, *args, **kwargs)
-        self.valid_open_values = list(map(self.lowerCaseString, self.valid_open_values))
-        self.valid_close_values = list(map(self.lowerCaseString, self.valid_close_values))
+    signal = Component(Signal, value=0)
     
     # - - - - likely to override these methods in subclass - - - -
 
@@ -360,87 +502,6 @@ class ShutterBase(Device):
             if self.delay_s > 0:
                 time.sleep(self.delay_s)    # blocking call OK here
 
-    def set(self, value, **kwargs):
-        """
-        plan: request the shutter to open or close
-
-        PARAMETERS
-        
-        value : str
-            any from ``self.choices`` (typically "open" or "close")
-        
-        kwargs : dict
-            ignored at this time
-
-        """
-        if self.busy.value:
-            raise RuntimeError("shutter is operating")
-        
-        __value__ = self.lowerCaseString(value)
-        self.validTarget(__value__)
-
-        status = DeviceStatus(self)
-        
-        if self.inPosition(__value__):
-            # no need to move, cut straight to the end
-            status._finished(success=True)
-        else:
-            def move_it():
-                # runs in a thread, no need to "yield from"
-                self.busy.put(True)
-                if __value__ in self.valid_open_values:
-                    self.open()
-                elif __value__ in self.valid_close_values:
-                    self.close()
-                self.busy.put(False)
-                status._finished(success=True)
-            # get it moving
-            threading.Thread(target=move_it, daemon=True).start()
-        return status
-    
-    # - - - - - - possible to override in subclass - - - - - -
-    
-    @property
-    def isOpen(self):
-        """is the shutter open?"""
-        return str(self.state) == self.valid_open_values[0]
-    
-    @property
-    def isClosed(self):
-        """is the shutter closed?"""
-        return str(self.state) == self.valid_close_values[0]
-
-    def inPosition(self, target):
-        """is the shutter at the target position?"""
-        self.validTarget(target)
-        __value__ = self.lowerCaseString(target)
-        if __value__ in self.valid_open_values and self.isOpen:
-            return True
-        elif __value__ in self.valid_close_values and self.isClosed:
-            return True
-        return False
-    
-    # - - - - - - not likely to override in subclass - - - - - -
-
-    def addCloseValue(self, text):
-        """a synonym to close the shutter, use with set()"""
-        self.valid_close_values.append(self.lowerCaseString(text))
-        return self.choices     # return the list of acceptable values
-    
-    def addOpenValue(self, text):
-        """a synonym to open the shutter, use with set()"""
-        self.valid_open_values.append(self.lowerCaseString(text))
-        return self.choices     # return the list of acceptable values
-
-    @property
-    def choices(self):
-        """return list of acceptable choices for set()"""
-        return self.valid_open_values + self.valid_close_values
-    
-    def lowerCaseString(self, value):
-        """ensure any given value is a lower-case string"""
-        return str(value).lower()
-
     @property
     def state(self):
         """is shutter "open", "close", or "unknown"?"""
@@ -451,21 +512,6 @@ class ShutterBase(Device):
         else:
             result = self.unknown_state
         return result
-
-    def validTarget(self, target, should_raise=True):
-        """
-        return whether (or not) target value is acceptable for self.set()
-        
-        raise ValueError if not acceptable (default)
-        """
-        acceptable_values = self.choices
-        ok = self.lowerCaseString(target) in acceptable_values
-        if not ok and should_raise:
-            msg = "received " + str(target)
-            msg += " : should be only one of "
-            msg += " | ".join(acceptable_values)
-            raise ValueError(msg)
-        return ok
 
 
 class ApsPssShutter(Device):
@@ -670,7 +716,7 @@ class ApsPssShutterWithStatus(Device):
         return self.pss_state.value == self.close_val
 
 
-class SimulatedApsPssShutterWithStatus(ShutterBase):
+class SimulatedApsPssShutterWithStatus(Device):
     """
     Simulated APS PSS shutter
     
