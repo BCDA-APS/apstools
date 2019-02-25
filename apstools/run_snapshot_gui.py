a55bf7bca5e497dea8ffba290326e3ecd96e63e9
Pete Jemian <prjemian@gmail.com>
2019-02-25 12:31:36 -0600

    make life a little easier for developer

--- apstools/run_snapshot_gui.py
+++ apstools/run_snapshot_gui.py
@@ -8,10 +8,23 @@ import sys
 sys.path.insert(0, "..")
  
 import apstools.snapshot
+from pymongo.errors import ServerSelectionTimeoutError
 
-config = "mongodb_config"
-# config = "mongodb_localhost"
-# config = "mongodb_poof"
+developer_config_candidates = [
+    "mongodb_config",
+    "mongodb_poof",
+    "mongodb_ookhd",
+    ]
 
 if __name__ == "__main__":
-    apstools.snapshot.snapshot_gui(config)
+    ext =  ".yml"
+    for config in developer_config_candidates:
+        print("Trying mongodb configuration: " + config + ext)
+        try:
+            apstools.snapshot.snapshot_gui(config)
+            break
+        except FileNotFoundError as exc:
+            print("Mongodb configuration file not found: " + config + ext)
+            print(exc)
+        except ServerSelectionTimeoutError:
+            print("Timeout when cennecting with mongodb server: " + config + ext)
