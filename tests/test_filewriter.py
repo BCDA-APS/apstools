3e06fb9184ab0d90a927d0d845a5ab4b1eb8153e
Pete Jemian <prjemian@gmail.com>
2019-05-09 00:16:24 -0500

    #128 WIP more tests of specwriter

--- tests/test_filewriter.py
+++ tests/test_filewriter.py
@@ -3,42 +3,39 @@
 unit tests for the SPEC filewriter
 """
 
+import json
 import os
+import shutil
 import sys
+import tempfile
 import unittest
+import zipfile
 
 _test_path = os.path.dirname(__file__)
 _path = os.path.join(_test_path, '..')
 if _path not in sys.path:
     sys.path.insert(0, _path)
 
-import apstools
 from apstools.filewriters import SpecWriterCallback
-from databroker import Broker
-import json
-import shutil
-import tempfile
-import zipfile
 
-
-"""
-sqlite fails with this error:
-
-  File "/home/mintadmin/Apps/anaconda/envs/bluesky/lib/python3.6/site-packages/databroker/headersource/sqlite.py", line 257, in _insert_events
-    values[desc_uid])
-sqlite3.InterfaceError: Error binding parameter 107 - probably unsupported type.
-
-Can we pick a different backend for temporary work?
-"""
 
 ZIP_FILE = os.path.join(_test_path, "usaxs_docs.json.zip")
 JSON_FILE = "usaxs_docs.json.txt"
 
 
+def write_stream(specwriter, stream):
+    """write the doc stream to the file"""
+    for document in stream:
+        tag, doc = document
+        specwriter.receiver(tag, doc)
+
+
 class Test_Data_is_Readable(unittest.TestCase):
     
     def test_00_testfile_exists(self):
-        self.assertTrue(os.path.exists(ZIP_FILE), "zip file with test data")
+        self.assertTrue(
+            os.path.exists(ZIP_FILE), 
+            "zip file with test data")
         
         # get our test document stream
         with zipfile.ZipFile(ZIP_FILE, "r") as fp:
@@ -54,10 +51,16 @@ class Test_Data_is_Readable(unittest.TestCase):
         
         # test that tune_mr content arrived intact
         keys = dict(start=1, descriptor=2, event=33, stop=1)
-        self.assertEqual(len(census.keys()), len(keys), "four document types")
+        self.assertEqual(
+            len(census.keys()), 
+            len(keys), 
+            "four document types")
         for k, v in keys.items():
             self.assertIn(k, census, f"{k} document exists")
-            self.assertEqual(census[k], v, f"expected {v} '{k}' document(s)")
+            self.assertEqual(
+                census[k], 
+                v, 
+                f"expected {v} '{k}' document(s)")
 
 
 class Test_newfile(unittest.TestCase):
@@ -80,18 +83,55 @@ class Test_newfile(unittest.TestCase):
         testfile = os.path.join(self.tempdir, "tune_mr.dat")
         specwriter = SpecWriterCallback(filename=testfile)
 
-        self.assertIsInstance(specwriter, SpecWriterCallback, "specwriter object")
-        self.assertEqual(specwriter.spec_filename, testfile, "output data file")
-        self.assertFalse(os.path.exists(testfile), "data file not created yet")
+        self.assertIsInstance(
+            specwriter, SpecWriterCallback, 
+            "specwriter object")
+        self.assertEqual(
+            specwriter.spec_filename, 
+            testfile, 
+            "output data file")
 
-        # write the doc stream to the file
-        for document in self.db["tune_mr"]:
-            tag, doc = document
-            specwriter.receiver(tag, doc)
-
+        self.assertFalse(
+            os.path.exists(testfile), 
+            "data file not created yet")
+        write_stream(
+            specwriter, self.db["tune_mr"])
         self.assertTrue(os.path.exists(testfile), "data file created")
         
-        # TODO: test newfile() with existing file
+        # TODO: #128 do not raise if exists
+        try:
+            specwriter.newfile(filename=testfile)
+            raised = False
+        except ValueError:
+            raised = True
+        finally:
+            self.assertTrue(raised, "file exists")
+
+        specwriter = SpecWriterCallback()
+        path = os.path.abspath(
+            os.path.dirname(
+                specwriter.spec_filename))
+        self.assertNotEqual(
+            path, 
+            self.tempdir, 
+            "default file not in tempdir")
+        self.assertEqual(
+            path, 
+            os.path.abspath(os.getcwd()), 
+            "default file in pwd")
+
+        # change the directory
+        specwriter.spec_filename = os.path.join(
+            self.tempdir, 
+            specwriter.spec_filename)
+
+        self.assertFalse(
+            os.path.exists(specwriter.spec_filename), 
+            "data file not created yet")
+        write_stream(specwriter, self.db["tune_mr"])
+        self.assertTrue(
+            os.path.exists(specwriter.spec_filename), 
+            "data file created")
 
 
 def suite(*args, **kw):
