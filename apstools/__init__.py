4563abd4f2f3c125ccc14f0ca8ed0c8e8a29bb85
Pete Jemian <prjemian@gmail.com>
2019-07-04 22:26:12 -0500

    prepare to tag next release 1.1.7

--- apstools/__init__.py
+++ apstools/__init__.py
@@ -38,7 +38,6 @@ __classifiers__ = [
     'License :: Public Domain',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Topic :: Scientific/Engineering',
