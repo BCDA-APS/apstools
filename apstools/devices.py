de82e1a8a55b29d03a450e962d085918ee8ba4bd
Pete Jemian <prjemian@gmail.com>
2019-07-30 14:41:07 -0500

    FIX #209 Thanks, LGTM

--- apstools/devices.py
+++ apstools/devices.py
@@ -1687,7 +1687,7 @@ class AD_EpicsHdf5FileName(FileStorePluginBase):
     """
 
     def __init__(self, *args, **kwargs):
-        FileStorePluginBase.__init__(*args, **kwargs)
+        FileStorePluginBase.__init__(self, *args, **kwargs)
         self.filestore_spec = 'AD_HDF5'  # spec name stored in resource doc
         self.stage_sigs.update([
             ('file_template', '%s%s_%4.4d.h5'),
