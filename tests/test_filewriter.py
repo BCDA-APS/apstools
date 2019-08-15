7f5a324ef423de8197ca4e6f8a3f13174a96e804
Pete Jemian <prjemian@gmail.com>
2019-08-15 11:09:38 -0500

    TST #226

--- tests/test_filewriter.py
+++ tests/test_filewriter.py
@@ -119,29 +119,18 @@ class Test_SpecWriterCallback(unittest.TestCase):
         self.assertIn("108", scans)
         scan = sdf.getScan(108)
         self.assertEqual(scan.N[0], len(scan.L))
-
+        
         self.assertGreater(scan.header.raw.find("\n#O0 \n"), 0)
         self.assertGreater(scan.header.raw.find("\n#o0 \n"), 0)
-        
-        # see: https://github.com/prjemian/spec2nexus/issues/196
-        # tests/test_plugin.py  test_empty_positioner()
         self.assertEqual(len(scan.header.O), 1)
-        self.assertEqual(scan.header.O[0], [''])
+        self.assertEqual(len(scan.header.O[0]), 0)
+        self.assertEqual(len(scan.header.o), 1)
+        self.assertEqual(len(scan.header.o[0]), 0)
+
         self.assertGreater(scan.raw.find("\n#P0 \n"), 0)
         self.assertEqual(len(scan.P), 1)
-        self.assertEqual(scan.P[0], '')
-
-        # TODO: after next spec2nexus release 
-        # self.assertGreater(scan.header.raw.find("\n#O0 \n"), 0)
-        # self.assertGreater(scan.header.raw.find("\n#o0 \n"), 0)
-        # self.assertEqual(len(scan.header.O), 1)
-        # self.assertEqual(len(scan.header.O[0]), 0)
-        # self.assertEqual(len(scan.header.o), 1)
-        # self.assertEqual(len(scan.header.o[0]), 0)
-        # self.assertGreater(scan.raw.find("\n#P0 \n"), 0)
-        # self.assertEqual(len(scan.P), 1)
-        # self.assertEqual(len(scan.P[0]), 0)
-        # self.assertEqual(len(scan.positioner), 0)
+        self.assertEqual(len(scan.P[0]), 0)
+        self.assertEqual(len(scan.positioner), 0)
 
     def test_writer_filename(self):
         self.assertTrue(len(self.db) > 0, "test data ready")
