2499ec25fd5513d7bb299a231baab9a178f7704a
Pete Jemian <prjemian@gmail.com>
2020-01-31 16:07:09 -0600

    MNT #279

--- apstools/synApps/__init__.py
+++ apstools/synApps/__init__.py
@@ -7,14 +7,14 @@ synApps template XXX IOC.
 
 EXAMPLES:;
 
-    import apstools.synApps_ophyd
-    scans = apstools.synApps_ophyd.SscanDevice("xxx:", name="scans")
-    calcs = apstools.synApps_ophyd.userCalcsDevice("xxx:", name="calcs")
+    import apstools.synApps
+    scans = apstools.synApps.SscanDevice("xxx:", name="scans")
+    calcs = apstools.synApps.userCalcsDevice("xxx:", name="calcs")
 
     calc1 = calcs.calc1
-    apstools.synApps_ophyd.swait_setup_random_number(calc1)
+    apstools.synApps.swait_setup_random_number(calc1)
 
-    apstools.synApps_ophyd.swait_setup_incrementer(calcs.calc2)
+    apstools.synApps.swait_setup_incrementer(calcs.calc2)
     
     calc1.reset()
 
