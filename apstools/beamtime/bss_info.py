4e539326525d07680f34b54318459f59cf788fa4
Pete Jemian <prjemian@gmail.com>
2020-06-26 10:34:26 -0500

    ENH #333 initial commit

---
+++ apstools/beamtime/bss_info.py
@@ -0,0 +1,459 @@
+#!/usr/bin/env python
+
+"""
+Retrieve specific records from the APS Proposal and ESAF databases
+
+BSS: Beamline Scheduling System
+
+EXAMPLES::
+
+    bss_info current
+    bss_info esaf 226319
+    bss_info proposal 66083 2020-2 9-ID-B,C
+"""
+
+import argparse
+import datetime
+import dm               # APS data management library
+from bss_info_ophyd import EpicsBssDevice
+import os
+import pyRestTable
+import sys
+import time
+import yaml
+
+DM_APS_DB_WEB_SERVICE_URL = "https://xraydtn01.xray.aps.anl.gov:11236"
+CONNECT_TIMEOUT = 5
+POLL_INTERVAL = 0.01
+
+api_bss = dm.BssApsDbApi(DM_APS_DB_WEB_SERVICE_URL)
+api_esaf = dm.EsafApsDbApi(DM_APS_DB_WEB_SERVICE_URL)
+
+_cache_ = {}
+
+
+class EpicsNotConnected(Exception): ...
+
+
+def connect_epics(prefix):
+    t0 = time.time()
+    t_timeout = t0 + CONNECT_TIMEOUT
+    bss = EpicsBssDevice(prefix, name="bss")
+    while not bss.connected and time.time() < t_timeout:
+        time.sleep(POLL_INTERVAL)
+    # printColumns(bss.read_attrs, width=40, numColumns=2)
+    if not bss.connected:
+        raise EpicsNotConnected(
+            f"Did not connect with EPICS {prefix} in {CONNECT_TIMEOUT}s")
+    t_connect = time.time() - t0
+    print(f"connected in {t_connect:.03f}s")
+    return bss
+
+
+def epicsClear(prefix):
+    """
+    clear the EPICS database
+    """
+    print(f"clear EPICS {prefix}")
+    bss = connect_epics(prefix)
+
+    t0 = time.time()
+    bss.clear()
+    t_clear = time.time() - t0
+    print(f"cleared in {t_clear:.03f}s")
+
+
+def epicsUpdate(prefix):
+    """
+    update an EPICS database with current ESAF & proposal information
+    """
+    print(f"update EPICS {prefix}")
+    bss = connect_epics(prefix)
+
+    bss.clear()
+    cycle = bss.esaf.aps_cycle.get()
+    beamline = bss.proposal.beamline_name.get()
+    sector = bss.esaf.sector.get()
+    esaf_id = bss.esaf.esaf_id.get()
+    proposal_id = bss.proposal.proposal_id.get()
+
+    if len(beamline) == 0:
+        raise ValueError(
+            f"must set beamline name in {bss.proposal.beamline_name.pvname}")
+    elif beamline not in listAllBeamlines():
+        raise ValueError(f"{beamline} is not known")
+    if len(cycle) == 0:
+        raise ValueError(
+            f"must set APS cycle name in {bss.esaf.aps_cycle.pvname}")
+    elif cycle not in listAllRuns():
+        raise ValueError(f"{cycle} is not known")
+
+    bss.clear()
+
+    if len(esaf_id) > 0:
+        esaf = getEsaf(esaf_id)
+        bss.esaf.description.put(esaf["description"])
+        bss.esaf.title.put(esaf["esafTitle"])
+        bss.esaf.esaf_status.put(esaf["esafStatus"])
+        bss.esaf.end_date.put(esaf["experimentEndDate"])
+        bss.esaf.start_date.put(esaf["experimentStartDate"])
+
+        bss.esaf.user_last_names.put(
+            ",".join([user["lastName"] for user in esaf["experimentUsers"]])
+        )
+        bss.esaf.user_badges.put(
+            ",".join([user["badge"] for user in esaf["experimentUsers"]])
+        )
+        for i, user in enumerate(esaf["experimentUsers"]):
+            obj = getattr(bss.esaf, f"user{i+1}")
+            obj.badge_number.put(user["badge"])
+            obj.email.put(user["email"])
+            obj.first_name.put(user["firstName"])
+            obj.last_name.put(user["lastName"])
+            if i == 9:
+                break
+
+    if len(proposal_id) > 0:
+        proposal = getProposal(proposal_id, cycle, beamline)
+        bss.proposal.mail_in_flag.put(proposal["mailInFlag"] in ("Y", "y"))
+        bss.proposal.proprietary_flag.put(proposal["proprietaryFlag"] in ("Y", "y"))
+        bss.proposal.submitted_date.put(proposal["submittedDate"])
+        bss.proposal.title.put(proposal["title"])
+
+        bss.proposal.user_last_names.put(
+            ",".join([user["lastName"] for user in proposal["experimenters"]])
+        )
+        bss.proposal.user_badges.put(
+            ",".join([user["badge"] for user in proposal["experimenters"]])
+        )
+        for i, user in enumerate(proposal["experimenters"]):
+            obj = getattr(bss.proposal, f"user{i+1}")
+            obj.badge_number.put(user["badge"])
+            obj.email.put(user["email"])
+            obj.first_name.put(user["firstName"])
+            obj.last_name.put(user["lastName"])
+            obj.institution.put(user["institution"])
+            obj.institution_id.put(str(user["instId"]))
+            obj.user_id.put(str(user["id"]))
+            obj.pi_flag.put(user.get("piFlag") in ("Y", "y"))
+            if i == 9:
+                break
+
+
+def epicsSetup(prefix, beamline, cycle=None):
+    if beamline not in listAllBeamlines():
+        raise ValueError(f"{beamline} is not known")
+    if cycle not in listAllRuns():
+        raise ValueError(f"{cycle} is not known")
+
+    bss = connect_epics(prefix)
+
+    cycle = cycle or getCurrentCycle()
+    sector = int(beamline.split("-")[0])
+    print(f"setup EPICS {prefix} {beamline} cycle={cycle} sector={sector}")
+
+    bss.clear()
+    bss.esaf.aps_cycle.put(cycle)
+    bss.proposal.beamline_name.put(beamline)
+    bss.esaf.sector.put(str(sector))
+
+
+def getCurrentCycle():
+    return api_bss.getCurrentRun()["name"]
+
+
+def getCurrentEsafs(sector):
+    if isinstance(sector, int):
+        sector = f"{sector:02d}"
+    if len(sector) == 1:
+        sector = "0" + sector
+    tNow = datetime.datetime.now()
+    esafs = api_esaf.listEsafs(sector=sector, year=tNow.year)
+    results = []
+    for esaf in esafs:
+        if tNow < iso2datetime(esaf["experimentStartDate"]):
+            continue
+        if tNow > iso2datetime(esaf["experimentEndDate"]):
+            continue
+        results.append(esaf)
+    return results
+
+
+def getCurrentInfo(beamline):
+    sector = beamline.split("-")[0]
+    tNow = datetime.datetime.now()
+
+    matches = []
+    for esaf in api_esaf.listEsafs(sector=sector, year=tNow.year):
+        print(f"ESAF {esaf['esafId']}: {esaf['esafTitle']}")
+        esaf_badges = [user["badge"] for user in esaf["experimentUsers"]]
+        for run in listRecentRuns():
+            for proposal in api_bss.listProposals(beamlineName=beamline, runName=run):
+                print(f"proposal {proposal['id']}: {proposal['title']}")
+                proposal["experimenters"]
+                user["badge"]
+                count = 0
+                for user in proposal["experimenters"]:
+                    if user["badge"] in esaf_badges:
+                        count += 1
+                if count > 0:
+                    matches.append(
+                        dict(
+                            esaf=esaf, 
+                            proposal=proposal, 
+                            num_true=count, 
+                            num_esaf_badges=len(esaf_badges), 
+                            num_proposal_badges=len(findings),
+                        )
+                    )
+    return matches
+
+
+def getCurrentProposals(beamline):
+    proposals = []
+    for cycle in listRecentRuns():
+        for prop in api_bss.listProposals(beamlineName=beamline, runName=cycle):
+            prop = dict(prop)
+            prop["cycle"] = cycle
+            proposals.append(prop)
+    return proposals
+
+
+def getEsaf(esafId):
+    try:
+        record = api_esaf.getEsaf(int(esafId))
+    except dm.ObjectNotFound:
+        raise EsafNotFound(esafId)
+    return dict(record.data)
+
+
+def getProposal(proposalId, runName, beamlineName):
+    # avoid possible dm.DmException
+    if runName not in listAllRuns():
+        raise DmRecordNotFound(f"run '{runName}' not found")
+
+    if beamlineName not in listAllBeamlines():
+        raise DmRecordNotFound(f"beamline '{beamlineName}' not found")
+
+    try:
+        record = api_bss.getProposal(str(proposalId), runName, beamlineName)
+    except dm.ObjectNotFound:
+        raise ProposalNotFound(
+            f"id={proposalId}"
+            f" run={runName}"
+            f" beamline={beamlineName}"
+            )
+    return dict(record.data)
+
+
+def iso2datetime(isodate):
+    return datetime.datetime.fromisoformat(isodate)
+
+
+def listAllBeamlines():
+    if "beamlines" not in _cache_:
+        _cache_["beamlines"] = [
+            entry["name"]
+            for entry in api_bss.listBeamlines()
+        ]
+    return _cache_["beamlines"]
+
+
+def listAllRuns():
+    if "cycles" not in _cache_:
+        _cache_["cycles"] = sorted([
+            entry["name"]
+            for entry in api_bss.listRuns()
+        ])
+    return _cache_["cycles"]
+
+
+def listRecentRuns(quantity=6):
+    # 6 runs is the duration of a user proposal
+    tNow = datetime.datetime.now()
+    runs = [
+        run["name"]
+        for run in api_bss.listRuns()
+        if iso2datetime(run["startTime"]) <= tNow
+    ]
+    return sorted(runs, reverse=True)[:quantity]
+
+
+def printColumns(items, numColumns=5, width=10):
+    n = len(items)
+    rows = n // numColumns
+    if n % numColumns > 0:
+        rows += 1
+    for base in range(0, rows):
+        row = [
+            items[base+k*rows]
+            for k in range(numColumns) 
+            if base+k*rows < n]
+        print("".join([f"{s:{width}s}" for s in row]))
+
+
+def trim(text, length=40):
+    if len(text) > length:
+        text = text[:length-3] + "..."
+    return text
+
+
+class DmRecordNotFound(Exception): ...
+class EsafNotFound(DmRecordNotFound): ...
+class ProposalNotFound(DmRecordNotFound): ...
+
+
+class EsafInfo:
+    
+    def __init__(self, esaf):
+        self.title = esaf["esafTitle"]
+        self.id = esaf["esafId"]
+        self.status = esaf["esafStatus"]
+        self.startDate = esaf["experimentStartDate"]
+        self.endDate = esaf["experimentEndDate"]
+        self.sector = esaf["sector"]
+        users = [u["lastName"] for u in esaf["experimentUsers"]]
+        self.users = ",".join(sorted(users))
+
+
+class ScheduleInfo:
+    
+    def __init__(self, proposal):
+        self.others = []
+        self.contact = None
+        self.title = proposal["title"]
+        self.id = proposal["id"]
+        for exp in proposal["experimenters"]:
+            if exp.get("piFlag", "N") == "Y":
+                self.badge = exp["badge"]
+                self.email = exp["email"]
+                self.institution = exp["institution"]
+                self.contact = f'{exp["firstName"]} {exp["lastName"]}'
+            else:
+                self.others.append(exp["lastName"])
+        self.others = ",".join(sorted(self.others))
+
+
+def get_options():
+    parser = argparse.ArgumentParser(
+        prog=os.path.split(sys.argv[0])[-1], 
+        description=__doc__.strip().splitlines()[0],
+        )
+
+    subcommand = parser.add_subparsers(dest='subcommand', title='subcommand')
+
+    p_sub = subcommand.add_parser('beamlines', help="print list of beamlines")
+
+    p_sub = subcommand.add_parser('current', help="print current ESAF(s) and proposal(s)")
+    p_sub.add_argument('beamlineName', type=str, help="Beamline name")
+
+    p_sub = subcommand.add_parser('cycles', help="print APS cycle names")
+
+    p_sub = subcommand.add_parser('esaf', help="print specific ESAF")
+    p_sub.add_argument('esafId', type=int, help="ESAF ID number")
+
+    p_sub = subcommand.add_parser('proposal', help="print specific proposal")
+    p_sub.add_argument('proposalId', type=str, help="ESAF ID number")
+    p_sub.add_argument('cycle', type=str, help="APS run (cycle) name")
+    p_sub.add_argument('beamlineName', type=str, help="Beamline name")
+
+    p_sub = subcommand.add_parser('clear', help="EPICS PVs: clear")
+    p_sub.add_argument('prefix', type=str, help="EPICS PV prefix")
+
+    p_sub = subcommand.add_parser('setup', help="EPICS PVs: setup")
+    p_sub.add_argument('prefix', type=str, help="EPICS PV prefix")
+    p_sub.add_argument('beamlineName', type=str, help="Beamline name")
+    p_sub.add_argument('cycle', type=str, help="APS run (cycle) name")
+
+    p_sub = subcommand.add_parser('update', help="EPICS PVs: update from BSS")
+    p_sub.add_argument('prefix', type=str, help="EPICS PV prefix")
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_options()
+    if args.subcommand == "beamlines":
+        printColumns(listAllBeamlines(), numColumns=4, width=15)
+
+    elif args.subcommand == "clear":
+        epicsClear(args.prefix)
+
+    elif args.subcommand == "current":
+        records = getCurrentProposals(args.beamlineName)
+        if len(records) == 0:
+            print(f"No current proposals for {args.beamlineName}")
+        else:
+            table = pyRestTable.Table()
+            table.labels = "id cycle date user(s) title".split()
+            for item in records:
+                users = trim(",".join([
+                    user["lastName"]
+                    for user in item["experimenters"]
+                ]), 20)
+                table.addRow((
+                    item["id"], 
+                    item["cycle"], 
+                    item["submittedDate"], 
+                    users, 
+                    trim(item["title"]),))
+            print(f"Current Proposal(s) on {args.beamlineName}")
+            print()
+            print(table)
+
+        sector = args.beamlineName.split("-")[0]
+        records = getCurrentEsafs(sector)
+        if len(records) == 0:
+            print(f"No current ESAFs for sector {sector}")
+        else:
+            table = pyRestTable.Table()
+            table.labels = "id status start end user(s) title".split()
+            for item in records:
+                users = trim(
+                        ",".join([
+                        user["lastName"]
+                        for user in item["experimentUsers"]
+                    ]),
+                    20)
+                table.addRow((
+                    item["esafId"], 
+                    item["esafStatus"], 
+                    item["experimentStartDate"].split()[0],
+                    item["experimentEndDate"].split()[0],
+                    users, 
+                    trim(item["esafTitle"], 40),
+                    ))
+            print(f"Current ESAF(s) on sector {sector}")
+            print()
+            print(table)
+
+    elif args.subcommand == "cycles":
+        printColumns(listAllRuns())
+
+    elif args.subcommand == "esaf":
+        try:
+            esaf = getEsaf(args.esafId)
+            print(yaml.dump(esaf))
+        except DmRecordNotFound as exc:
+            print(exc)
+        except dm.DmException as exc:
+            print(f"dm reported: {exc}")
+
+    elif args.subcommand == "proposal":
+        try:
+            proposal = getProposal(args.proposalId, args.cycle, args.beamlineName)
+            print(yaml.dump(proposal))
+        except DmRecordNotFound as exc:
+            print(exc)
+        except dm.DmException as exc:
+            print(f"dm reported: {exc}")
+
+    elif args.subcommand == "setup":
+        epicsSetup(args.prefix, args.beamlineName, args.cycle)
+
+    elif args.subcommand == "update":
+        epicsUpdate(args.prefix)
+
+
+if __name__ == "__main__":
+    main()
---
+++ apstools/beamtime/bss_info_ophyd.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python
+
+"""
+ophyd support for bss_info
+
+EXAMPLE::
+
+    bss_info = EpicsBssDevice("ioc:bss:", name="bss_info")
+"""
+
+__all__ = ["EpicsBssDevice",]
+
+from ophyd import Component, Device, EpicsSignal
+
+
+class EpicsEsafExperimenterDevice(Device):
+    """
+    - badge: '64065'
+        badgeNumber: '64065'
+        email: kuzmenko@aps.anl.gov
+        firstName: Ivan
+        lastName: Kuzmenko
+    """
+    badge_number = Component(EpicsSignal, "badgeNumber", string=True)
+    email = Component(EpicsSignal, "email", string=True)
+    first_name = Component(EpicsSignal, "firstName", string=True)
+    last_name = Component(EpicsSignal, "lastName", string=True)
+
+    def clear(self):
+        self.badge_number.put("")
+        self.email.put("")
+        self.first_name.put("")
+        self.last_name.put("")
+
+
+class EpicsEsafDevice(Device):
+    """
+    description: We will commission beamline and  USAXS instrument. We will perform experiments
+        with safe beamline standards and test samples (all located at beamline and used
+        for this purpose routinely) to evaluate performance of beamline and instrument.
+        We will perform hardware and software development as needed.
+    esafId: 226319
+    esafStatus: Approved
+    esafTitle: Commission 9ID and USAXS
+    experimentEndDate: '2020-09-28 08:00:00'
+    experimentStartDate: '2020-05-26 08:00:00'
+    experimentUsers:
+    - badge: '86312'
+        badgeNumber: '86312'
+        email: ilavsky@aps.anl.gov
+        firstName: Jan
+        lastName: Ilavsky
+    - badge: '53748'
+        badgeNumber: '53748'
+        email: emaxey@aps.anl.gov
+        firstName: Evan
+        lastName: Maxey
+    - badge: '64065'
+        badgeNumber: '64065'
+        email: kuzmenko@aps.anl.gov
+        firstName: Ivan
+        lastName: Kuzmenko
+    sector: 09
+    """
+
+    aps_cycle = Component(EpicsSignal, "cycle", string=True)
+    description = Component(EpicsSignal, "description", string=True)
+    end_date = Component(EpicsSignal, "endDate", string=True)
+    esaf_id = Component(EpicsSignal, "id", string=True)
+    esaf_status = Component(EpicsSignal, "status", string=True)
+    sector = Component(EpicsSignal, "sector", string=True)
+    start_date = Component(EpicsSignal, "startDate", string=True)
+    title = Component(EpicsSignal, "title", string=True)
+    user_last_names = Component(EpicsSignal, "users", string=True)
+    user_badges = Component(EpicsSignal, "userBadges", string=True)
+
+    user1 = Component(EpicsEsafExperimenterDevice, "user1:")
+    user2 = Component(EpicsEsafExperimenterDevice, "user2:")
+    user3 = Component(EpicsEsafExperimenterDevice, "user3:")
+    user4 = Component(EpicsEsafExperimenterDevice, "user4:")
+    user5 = Component(EpicsEsafExperimenterDevice, "user5:")
+    user6 = Component(EpicsEsafExperimenterDevice, "user6:")
+    user7 = Component(EpicsEsafExperimenterDevice, "user7:")
+    user8 = Component(EpicsEsafExperimenterDevice, "user8:")
+    user9 = Component(EpicsEsafExperimenterDevice, "user9:")
+
+    def clear(self):
+        # self.aps_cycle.put("")    # user controls this
+        self.description.put("")
+        self.end_date.put("")
+        # self.esaf_id.put("")      # user controls this
+        self.esaf_status.put("")
+        # self.sector.put("")
+        self.start_date.put("")
+        self.title.put("")
+        self.user_last_names.put("")
+        self.user_badges.put("")
+
+        self.clear_users()
+
+    def clear_users(self):
+        self.user1.clear()
+        self.user2.clear()
+        self.user3.clear()
+        self.user4.clear()
+        self.user5.clear()
+        self.user6.clear()
+        self.user7.clear()
+        self.user8.clear()
+        self.user9.clear()
+
+
+class EpicsProposalExperimenterDevice(Device):
+    """
+    badge: '300679'
+    email: yilianglin@uchicago.edu
+    firstName: Yiliang
+    id: 433796
+    instId: 3435
+    institution: The University of Chicago
+    lastName: Lin
+    piFlag: Y
+    """
+    badge_number = Component(EpicsSignal, "badgeNumber", string=True)
+    email = Component(EpicsSignal, "email", string=True)
+    first_name = Component(EpicsSignal, "firstName", string=True)
+    institution = Component(EpicsSignal, "institution", string=True)
+    institution_id = Component(EpicsSignal, "instId", string=True)
+    last_name = Component(EpicsSignal, "lastName", string=True)
+    pi_flag = Component(EpicsSignal, "piFlag", string=True)
+    user_id = Component(EpicsSignal, "userId", string=True)
+
+    def clear(self):
+        self.badge_number.put("")
+        self.email.put("")
+        self.first_name.put("")
+        self.last_name.put("")
+        self.user_id.put("")
+        self.institution_id.put("")
+        self.institution.put("")
+        self.pi_flag.put(0)
+
+
+class EpicsProposalDevice(Device):
+    """
+    experimenters:
+    - badge: '292588'
+        email: fangyin123@uchicago.edu
+        firstName: Yin
+        id: 433774
+        instId: 3435
+        institution: The University of Chicago
+        lastName: Fang
+    - badge: '304975'
+        email: sjiuyun@uchicago.edu
+        firstName: Jiuyun
+        id: 433775
+        instId: 3435
+        institution: The University of Chicago
+        lastName: Shi
+    - badge: '300679'
+        email: yilianglin@uchicago.edu
+        firstName: Yiliang
+        id: 433796
+        instId: 3435
+        institution: The University of Chicago
+        lastName: Lin
+        piFlag: Y
+    id: 66083
+    mailInFlag: N
+    proprietaryFlag: N
+    submittedDate: '2019-07-04 13:42:32'
+    title: Mapping the mechanical-responsive conductive network in bioinspired composite
+        materials with 3D correlative x-ray fluorescence and ptychographic tomography
+    totalShiftsRequested: 27
+    """
+
+    beamline_name = Component(EpicsSignal, "beamline", string=True)
+
+    mail_in_flag = Component(EpicsSignal, "mailInFlag", string=True)
+    proposal_id = Component(EpicsSignal, "id", string=True)
+    proprietary_flag = Component(EpicsSignal, "proprietaryFlag", string=True)
+    submitted_date = Component(EpicsSignal, "submittedDate", string=True)
+    title = Component(EpicsSignal, "title", string=True)
+    user_last_names = Component(EpicsSignal, "users", string=True)
+    user_badges = Component(EpicsSignal, "userBadges", string=True)
+
+    user1 = Component(EpicsProposalExperimenterDevice, "user1:")
+    user2 = Component(EpicsProposalExperimenterDevice, "user2:")
+    user3 = Component(EpicsProposalExperimenterDevice, "user3:")
+    user4 = Component(EpicsProposalExperimenterDevice, "user4:")
+    user5 = Component(EpicsProposalExperimenterDevice, "user5:")
+    user6 = Component(EpicsProposalExperimenterDevice, "user6:")
+    user7 = Component(EpicsProposalExperimenterDevice, "user7:")
+    user8 = Component(EpicsProposalExperimenterDevice, "user8:")
+    user9 = Component(EpicsProposalExperimenterDevice, "user9:")
+
+    def clear(self):
+        # self.beamline_name.put("")    # user controls this
+        self.mail_in_flag.put(0)
+        # self.proposal_id.put(-1)      # user controls this
+        self.proprietary_flag.put(0)
+        self.submitted_date.put("")
+        self.title.put("")
+        self.user_last_names.put("")
+        self.user_badges.put("")
+
+        self.clear_users()
+
+    def clear_users(self):
+        self.user1.clear()
+        self.user2.clear()
+        self.user3.clear()
+        self.user4.clear()
+        self.user5.clear()
+        self.user6.clear()
+        self.user7.clear()
+        self.user8.clear()
+        self.user9.clear()
+
+
+class EpicsBssDevice(Device):
+    esaf = Component(EpicsEsafDevice, "esaf:")
+    proposal = Component(EpicsProposalDevice, "proposal:")
+
+    def clear(self):
+        self.esaf.clear()
+        self.proposal.clear()
