de0413f98775006228fddbb63235927c2316964a
Pete R Jemian <prjemian@gmail.com>
2021-03-24 18:50:07 -0500

    TST #496

--- tests/debug_findpv.py
+++ tests/debug_findpv.py
@@ -4,15 +4,34 @@ examine the execution of utils.findpv()
 needs a connection to PVs, of course
 """
 
-from apstools.utils import findpv
+import apstools.utils
 import ophyd
+import sys
+import os
 
-m1 = ophyd.EpicsMotor("gp:m1", name="m1")
+sys.path.append(
+    os.path.abspath(
+        os.path.join(__file__, "..")
+    )
+)
+
+
+class MyCam(ophyd.SimDetectorCam):
+    pool_max_buffers = None
+
+
+class MyDetector(ophyd.SimDetector):
+    cam = ophyd.ADComponent(MyCam, "cam1:")
 
 
 def main():
-    choices = findpv("gp:m1.RBV")
-    print(f"{choices = }")
+    gpm1=ophyd.EpicsMotor("gp:m1", name="gpm1")
+    simdet = MyDetector("ad:", name="simdet")
+    gpm1.wait_for_connection()
+    simdet.wait_for_connection()
+    ns = dict(m1=gpm1, simdet=simdet)
+    print(f'{apstools.utils.findpv("gp:m1.RBV", ns=ns) = }')
+    print(f'{apstools.utils.findpv("ad:cam1:Acquire", ns=ns) = }')
 
 
 if __name__ == "__main__":
