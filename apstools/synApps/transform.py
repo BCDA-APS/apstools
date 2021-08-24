d44626dd9021be75eff55eca579326b2cf8af63d
Pete R Jemian <prjemian@gmail.com>
2021-08-24 12:26:26 -0500

    MNT #527 transform

--- apstools/synApps/transform.py
+++ apstools/synApps/transform.py
@@ -53,18 +53,18 @@ class transformRecordChannel(Device):
         ~reset
     """
 
-    current_value = FC(EpicsSignal, "{self.prefix}.{self._ch_letter}")
-    last_value = FC(EpicsSignalRO, "{self.prefix}.L{self._ch_letter}")
-    input_pv = FC(EpicsSignal, "{self.prefix}.INP{self._ch_letter}")
-    input_pv_valid = FC(EpicsSignalRO, "{self.prefix}.I{self._ch_letter}V")
+    current_value = FC(EpicsSignal, "{self.prefix}.{self._ch_letter}", kind="hinted")
+    last_value = FC(EpicsSignalRO, "{self.prefix}.L{self._ch_letter}", kind="config")
+    input_pv = FC(EpicsSignal, "{self.prefix}.INP{self._ch_letter}", kind="config")
+    input_pv_valid = FC(EpicsSignalRO, "{self.prefix}.I{self._ch_letter}V", kind="config")
     expression_invalid = FC(
-        EpicsSignalRO, "{self.prefix}.C{self._ch_letter}V"
+        EpicsSignalRO, "{self.prefix}.C{self._ch_letter}V", kind="config"
     )
-    comment = FC(EpicsSignal, "{self.prefix}.CMT{self._ch_letter}")
-    expression = FC(EpicsSignal, "{self.prefix}.CLC{self._ch_letter}")
-    output_pv = FC(EpicsSignal, "{self.prefix}.OUT{self._ch_letter}")
+    comment = FC(EpicsSignal, "{self.prefix}.CMT{self._ch_letter}", kind="config")
+    expression = FC(EpicsSignal, "{self.prefix}.CLC{self._ch_letter}", kind="config")
+    output_pv = FC(EpicsSignal, "{self.prefix}.OUT{self._ch_letter}", kind="config")
     output_pv_valid = FC(
-        EpicsSignalRO, "{self.prefix}.O{self._ch_letter}V"
+        EpicsSignalRO, "{self.prefix}.O{self._ch_letter}V", kind="config"
     )
 
     read_attrs = ["current_value"]
@@ -102,13 +102,13 @@ class TransformRecord(EpicsRecordDeviceCommonAll):
     :see: https://htmlpreview.github.io/?https://raw.githubusercontent.com/epics-modules/calc/R3-6-1/documentation/TransformRecord.html#Fields
     """
 
-    units = Cpt(EpicsSignal, ".EGU")
-    precision = Cpt(EpicsSignal, ".PREC")
-    version = Cpt(EpicsSignalRO, ".VERS")
+    units = Cpt(EpicsSignal, ".EGU", kind="config")
+    precision = Cpt(EpicsSignal, ".PREC", kind="config")
+    version = Cpt(EpicsSignalRO, ".VERS", kind="config")
 
-    calc_option = Cpt(EpicsSignal, ".COPT")
-    invalid_link_action = Cpt(EpicsSignalRO, ".IVLA")
-    input_bitmap = Cpt(EpicsSignalRO, ".MAP")
+    calc_option = Cpt(EpicsSignal, ".COPT", kind="config")
+    invalid_link_action = Cpt(EpicsSignalRO, ".IVLA", kind="config")
+    input_bitmap = Cpt(EpicsSignalRO, ".MAP", kind="config")
 
     read_attrs = APS_utils.itemizer("channels.%s", CHANNEL_LETTERS_LIST)
     hints = {"fields": read_attrs}
@@ -140,7 +140,7 @@ class UserTransformsDevice(Device):
     .. index:: Ophyd Device; synApps UserTransformsDevice
     """
 
-    enable = Cpt(EpicsSignal, "userTranEnable")
+    enable = Cpt(EpicsSignal, "userTranEnable", kind="config")
     transform1 = Cpt(TransformRecord, "userTran1")
     transform2 = Cpt(TransformRecord, "userTran2")
     transform3 = Cpt(TransformRecord, "userTran3")
