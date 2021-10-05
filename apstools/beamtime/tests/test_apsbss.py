dc207f09f040f481110cf2063b219bb454b29f07
Pete R Jemian <prjemian@gmail.com>
2021-10-04 23:48:25 -0500

    TST #447

--- apstools/beamtime/tests/test_apsbss.py
+++ apstools/beamtime/tests/test_apsbss.py
@@ -5,9 +5,14 @@ General tests of the apsbss module
 from .. import apsbss, apsbss_makedb
 
 import datetime
+import epics
 import os
-import uuid
+import pytest
 import socket
+import subprocess
+import sys
+import time
+import uuid
 
 BSS_TEST_IOC_PREFIX = f"tst{uuid.uuid4().hex[:7]}:bss:"
 
@@ -16,22 +21,24 @@ DATA_PATH = os.path.abspath(
 )
 
 
+@pytest.fixture(scope="function")
+def argv():
+    argv = [
+        "apsbss",
+    ]
+    return argv
+
+
 def using_APS_workstation():
     hostname = socket.gethostname()
     return hostname.lower().endswith(".aps.anl.gov")
 
 
 def bss_IOC_available():
-    import epics
-
     # try connecting with one of the PVs in the database
     cycle = epics.PV(f"{BSS_TEST_IOC_PREFIX}esaf:cycle")
     cycle.wait_for_connection(timeout=2)
     return cycle.connected
-
-
-def test_simple():
-    assert True
 
 
 def test_general():
@@ -98,221 +105,217 @@ def test_trim():
     assert got == "01234..."
 
 
-# class Test_EPICS(unittest.TestCase):
-#     def setUp(self):
-#         self.bss = None
-#         self.manager = os.path.abspath(
-#             os.path.join(os.path.dirname(apsbss.__file__), "apsbss_ioc.sh")
-#         )
-#         self.ioc_name = "test_apsbss"
-#         cmd = (
-#             f"{self.manager} restart {self.ioc_name} {BSS_TEST_IOC_PREFIX}"
-#         )
-#         self.ioc_process = subprocess.Popen(
-#             cmd.encode().split(),
-#             stdin=subprocess.PIPE,
-#             stdout=subprocess.PIPE,
-#             stderr=subprocess.PIPE,
-#             shell=False,
-#         )
-#         time.sleep(0.5)  # allow the IOC to start
-
-#     def tearDown(self):
-#         if self.bss is not None:
-#             self.bss.destroy()
-#             self.bss = None
-#         if self.ioc_process is not None:
-#             self.ioc_process = None
-#             cmd = f"{self.manager} stop {self.ioc_name} {BSS_TEST_IOC_PREFIX}"
-#             subprocess.Popen(
-#                 cmd.encode().split(),
-#                 stdin=subprocess.PIPE,
-#                 stdout=subprocess.PIPE,
-#                 stderr=subprocess.PIPE,
-#                 shell=False,
-#             )
-#             # self.ioc_process.communicate(cmd.encode().split())
-#             self.manager = None
-
-#     def test_ioc(self):
-#         if not bss_IOC_available():
-#             return
-
-#         from apstools.beamtime import apsbss_ophyd as bio
-
-#         self.bss = bio.EpicsBssDevice(BSS_TEST_IOC_PREFIX, name="bss")
-#         self.bss.wait_for_connection(timeout=2)
-#         self.assertTrue(self.bss.connected)
-#         self.assertEqual(self.bss.esaf.title.get(), "")
-#         self.assertEqual(self.bss.esaf.description.get(), "")
-#         # self.assertEqual(self.bss.esaf.aps_cycle.get(), "")
-
-#     def test_EPICS(self):
-#         from tests.common import Capture_stdout
-
-#         if not bss_IOC_available():
-#             return
-
-#         beamline = "9-ID-B,C"
-#         cycle = "2019-3"
-
-#         with Capture_stdout():
-#             self.bss = apsbss.connect_epics(BSS_TEST_IOC_PREFIX)
-#         self.assertTrue(self.bss.connected)
-#         self.assertEqual(self.bss.esaf.aps_cycle.get(), "")
-
-#         self.bss.esaf.aps_cycle.put(cycle)
-#         self.assertNotEqual(self.bss.esaf.aps_cycle.get(), "")
-
-#         if not using_APS_workstation():
-#             return
-
-#         # setup
-#         with Capture_stdout():
-#             apsbss.epicsSetup(BSS_TEST_IOC_PREFIX, beamline, cycle)
-#         self.assertNotEqual(self.bss.proposal.beamline_name.get(), "harpo")
-#         self.assertEqual(self.bss.proposal.beamline_name.get(), beamline)
-#         self.assertEqual(self.bss.esaf.aps_cycle.get(), cycle)
-#         self.assertEqual(
-#             self.bss.esaf.sector.get(), beamline.split("-")[0]
-#         )
-
-#         # epicsUpdate
-#         # Example ESAF on sector 9
-
-#         # ====== ======== ========== ========== ==================== =================================
-#         # id     status   start      end        user(s)              title
-#         # ====== ======== ========== ========== ==================== =================================
-#         # 226319 Approved 2020-05-26 2020-09-28 Ilavsky,Maxey,Kuz... Commission 9ID and USAXS
-#         # ====== ======== ========== ========== ==================== =================================
-
-#         # ===== ====== =================== ==================== ========================================
-#         # id    cycle  date                user(s)              title
-#         # ===== ====== =================== ==================== ========================================
-#         # 64629 2019-2 2019-03-01 18:35:02 Ilavsky,Okasinski    2019 National School on Neutron & X-r...
-#         # ===== ====== =================== ==================== ========================================
-#         esaf_id = "226319"
-#         proposal_id = "64629"
-#         self.bss.esaf.aps_cycle.put("2019-2")
-#         self.bss.esaf.esaf_id.put(esaf_id)
-#         self.bss.proposal.proposal_id.put(proposal_id)
-#         with Capture_stdout():
-#             apsbss.epicsUpdate(BSS_TEST_IOC_PREFIX)
-#         self.assertEqual(
-#             self.bss.esaf.title.get(), "Commission 9ID and USAXS"
-#         )
-#         self.assertTrue(
-#             self.bss.proposal.title.get().startswith(
-#                 "2019 National School on Neutron & X-r"
-#             )
-#         )
-
-#         with Capture_stdout():
-#             apsbss.epicsClear(BSS_TEST_IOC_PREFIX)
-#         self.assertNotEqual(self.bss.esaf.aps_cycle.get(), "")
-#         self.assertEqual(self.bss.esaf.title.get(), "")
-#         self.assertEqual(self.bss.proposal.title.get(), "")
+class IOC_ProcessConfig:
+    bss = None
+    manager = None
+    ioc_name = "test_apsbss"
+    ioc_process = None
 
 
-# class Test_MakeDatabase(unittest.TestCase):
-#     def test_general(self):
-#         from tests.common import Capture_stdout
+def run_process(cmd):
+    return subprocess.Popen(
+        cmd.encode().split(),
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=False,
+    )
 
-#         with Capture_stdout() as db:
-#             apsbss_makedb.main()
-#         self.assertEqual(len(db), 384)
-#         self.assertEqual(db[0], "#")
-#         self.assertEqual(db[1], "# file: apsbss.db")
-#         # randomly-selected spot checks
-#         self.assertEqual(db[13], 'record(stringout, "$(P)status")')
-#         self.assertEqual(db[28], 'record(stringout, "$(P)esaf:id")')
-#         self.assertEqual(db[138], '    field(ONAM, "ON")')
-#         self.assertEqual(
-#             db[285], 'record(bo, "$(P)proposal:user5:piFlag") {'
-#         )
+@pytest.fixture()
+def ioc():
+    # set up
+    cfg = IOC_ProcessConfig()
+
+    cfg.manager = os.path.abspath(
+        os.path.join(DATA_PATH, "..", "beamtime", "apsbss_ioc.sh")
+    )
+    cmd = f"{cfg.manager} restart {cfg.ioc_name} {BSS_TEST_IOC_PREFIX}"
+    cfg.ioc_process = run_process(cmd)
+    time.sleep(0.5)  # allow the IOC to start
+
+    # use
+    yield cfg
+
+    # tear down
+    if cfg.bss is not None:
+        cfg.bss.destroy()
+        cfg.bss = None
+    if cfg.ioc_process is not None:
+        cfg.ioc_process = None
+        cmd = f"{cfg.manager} stop {cfg.ioc_name} {BSS_TEST_IOC_PREFIX}"
+        run_process(cmd)
+        # self.ioc_process.communicate(cmd.encode().split())
+        cfg.manager = None
 
 
-# class Test_ProgramCommands(unittest.TestCase):
-#     def setUp(self):
-#         self.sys_argv0 = sys.argv[0]
-#         sys.argv = [
-#             self.sys_argv0,
-#         ]
+def test_ioc(ioc):
+    if not bss_IOC_available():
+        assert True  # assert *something*
+        return
 
-#     def tearDown(self):
-#         sys.argv = [
-#             self.sys_argv0,
-#         ]
+    from .. import apsbss_ophyd as bio
 
-#     def test_no_options(self):
-#         args = apsbss.get_options()
-#         self.assertIsNotNone(args)
-#         self.assertIsNone(args.subcommand)
+    ioc.bss = bio.EpicsBssDevice(BSS_TEST_IOC_PREFIX, name="bss")
+    ioc.bss.wait_for_connection(timeout=2)
+    assert ioc.bss.connected
+    assert ioc.bss.esaf.title.get() == ""
+    assert ioc.bss.esaf.description.get() == ""
+    # assert ioc.bss.esaf.aps_cycle.get() == ""
 
-#     def test_beamlines(self):
-#         sys.argv.append("beamlines")
-#         args = apsbss.get_options()
-#         self.assertIsNotNone(args)
-#         self.assertEqual(args.subcommand, "beamlines")
 
-#     def test_current(self):
-#         sys.argv.append("current")
-#         sys.argv.append("9-ID-B,C")
-#         args = apsbss.get_options()
-#         self.assertIsNotNone(args)
-#         self.assertEqual(args.subcommand, "current")
-#         self.assertEqual(args.beamlineName, "9-ID-B,C")
+def test_EPICS(ioc):
+    if not bss_IOC_available():
+        assert True  # assert *something*
+        return
 
-#     def test_cycles(self):
-#         sys.argv.append("cycles")
-#         args = apsbss.get_options()
-#         self.assertIsNotNone(args)
-#         self.assertEqual(args.subcommand, "cycles")
+    beamline = "9-ID-B,C"
+    cycle = "2019-3"
 
-#     def test_esaf(self):
-#         sys.argv.append("esaf")
-#         sys.argv.append("12345")
-#         args = apsbss.get_options()
-#         self.assertIsNotNone(args)
-#         self.assertEqual(args.subcommand, "esaf")
-#         self.assertEqual(args.esafId, 12345)
+    ioc.bss = apsbss.connect_epics(BSS_TEST_IOC_PREFIX)
+    assert ioc.bss.connected
+    assert ioc.bss.esaf.aps_cycle.get() == ""
 
-#     def test_proposal(self):
-#         sys.argv.append("proposal")
-#         sys.argv.append("proposal_number_here")
-#         sys.argv.append("1995-1")
-#         sys.argv.append("my_beamline")
-#         args = apsbss.get_options()
-#         self.assertIsNotNone(args)
-#         self.assertEqual(args.subcommand, "proposal")
-#         self.assertEqual(args.proposalId, "proposal_number_here")
-#         self.assertEqual(args.cycle, "1995-1")
-#         self.assertEqual(args.beamlineName, "my_beamline")
+    ioc.bss.esaf.aps_cycle.put(cycle)
+    assert ioc.bss.esaf.aps_cycle.get() != ""
 
-#     def test_EPICS_clear(self):
-#         sys.argv.append("clear")
-#         sys.argv.append("bss:")
-#         args = apsbss.get_options()
-#         self.assertIsNotNone(args)
-#         self.assertEqual(args.subcommand, "clear")
-#         self.assertEqual(args.prefix, "bss:")
+    if not using_APS_workstation():
+        return
 
-#     def test_EPICS_setup(self):
-#         sys.argv.append("setup")
-#         sys.argv.append("bss:")
-#         sys.argv.append("my_beamline")
-#         sys.argv.append("1995-1")
-#         args = apsbss.get_options()
-#         self.assertIsNotNone(args)
-#         self.assertEqual(args.subcommand, "setup")
-#         self.assertEqual(args.prefix, "bss:")
-#         self.assertEqual(args.beamlineName, "my_beamline")
-#         self.assertEqual(args.cycle, "1995-1")
+    # setup
+    apsbss.epicsSetup(BSS_TEST_IOC_PREFIX, beamline, cycle)
 
-#     def test_EPICS_update(self):
-#         sys.argv.append("update")
-#         sys.argv.append("bss:")
-#         args = apsbss.get_options()
-#         self.assertIsNotNone(args)
-#         self.assertEqual(args.subcommand, "update")
-#         self.assertEqual(args.prefix, "bss:")
+    assert ioc.bss.proposal.beamline_name.get() != "harpo"
+    assert ioc.bss.proposal.beamline_name.get() == beamline
+    assert ioc.bss.esaf.aps_cycle.get() == cycle
+    assert ioc.bss.esaf.sector.get() == beamline.split("-")[0]
+
+    # epicsUpdate
+    # Example ESAF on sector 9
+
+    # ====== ======== ========== ========== ==================== =================================
+    # id     status   start      end        user(s)              title
+    # ====== ======== ========== ========== ==================== =================================
+    # 226319 Approved 2020-05-26 2020-09-28 Ilavsky,Maxey,Kuz... Commission 9ID and USAXS
+    # ====== ======== ========== ========== ==================== =================================
+
+    # ===== ====== =================== ==================== ========================================
+    # id    cycle  date                user(s)              title
+    # ===== ====== =================== ==================== ========================================
+    # 64629 2019-2 2019-03-01 18:35:02 Ilavsky,Okasinski    2019 National School on Neutron & X-r...
+    # ===== ====== =================== ==================== ========================================
+    esaf_id = "226319"
+    proposal_id = "64629"
+    ioc.bss.esaf.aps_cycle.put("2019-2")
+    ioc.bss.esaf.esaf_id.put(esaf_id)
+    ioc.bss.proposal.proposal_id.put(proposal_id)
+    apsbss.epicsUpdate(BSS_TEST_IOC_PREFIX)
+    assert ioc.bss.esaf.title.get() == "Commission 9ID and USAXS"
+    assert ioc.bss.proposal.title.get().startswith(
+        "2019 National School on Neutron & X-r"
+    )
+
+    apsbss.epicsClear(BSS_TEST_IOC_PREFIX)
+    assert ioc.bss.esaf.aps_cycle.get() != ""
+    assert ioc.bss.esaf.title.get() == ""
+    assert ioc.bss.proposal.title.get() == ""
+
+
+def test_makedb(capsys):
+    apsbss_makedb.main()
+    db = capsys.readouterr().out.strip().split("\n")
+    assert len(db) == 384
+    random_spot_checks = {
+        0: "#",
+        1: "# file: apsbss.db",
+        13: 'record(stringout, "$(P)status")',
+        28: 'record(stringout, "$(P)esaf:id")',
+        138: '    field(ONAM, "ON")',
+        285: 'record(bo, "$(P)proposal:user5:piFlag") {',
+    }
+    for line_number, content in random_spot_checks.items():
+        assert db[line_number] == content
+
+
+def test_argv0(argv):
+    sys.argv = argv
+    assert len(sys.argv) == 1
+
+
+def test_apsbss_commands_no_options(argv):
+    sys.argv = argv
+    args = apsbss.get_options()
+    assert args is not None
+    assert args.subcommand is None
+
+
+def test_apsbss_commands_beamlines(argv):
+    sys.argv = argv + [
+        "beamlines",
+    ]
+    args = apsbss.get_options()
+    assert args is not None
+    assert args.subcommand == sys.argv[1]
+
+
+def test_apsbss_commands_current(argv):
+    sys.argv = argv + ["current", "9-ID-B,C"]
+    args = apsbss.get_options()
+    assert args is not None
+    assert args.subcommand == sys.argv[1]
+    assert args.beamlineName == sys.argv[2]
+
+
+def test_apsbss_commands_cycles(argv):
+    sys.argv = argv + [
+        "cycles",
+    ]
+    args = apsbss.get_options()
+    assert args is not None
+    assert args.subcommand == sys.argv[1]
+
+
+def test_apsbss_commands_esaf(argv):
+    sys.argv = argv + [
+        "esaf",
+        "12345",
+    ]
+    args = apsbss.get_options()
+    assert args is not None
+    assert args.subcommand == sys.argv[1]
+    assert args.esafId == 12345
+
+
+def test_apsbss_commands_proposal(argv):
+    sys.argv = argv + ["proposal", "proposal_number_here", "1995-1", "my_beamline"]
+    args = apsbss.get_options()
+    assert args is not None
+    assert args.subcommand == sys.argv[1]
+    assert args.proposalId == sys.argv[2]
+    assert args.cycle == sys.argv[3]
+    assert args.beamlineName == sys.argv[4]
+
+
+def test_apsbss_commands_EPICS_clear(argv):
+    sys.argv = argv + ["clear", "bss:"]
+    args = apsbss.get_options()
+    assert args is not None
+    assert args.subcommand == sys.argv[1]
+    assert args.prefix == sys.argv[2]
+
+
+def test_apsbss_commands_EPICS_setup(argv):
+    sys.argv = argv + ["setup", "bss:", "my_beamline", "1995-1"]
+    args = apsbss.get_options()
+    assert args is not None
+    assert args.subcommand == sys.argv[1]
+    assert args.prefix == sys.argv[2]
+    assert args.beamlineName == sys.argv[3]
+    assert args.cycle == sys.argv[4]
+
+
+def test_apsbss_commands_EPICS_update(argv):
+    sys.argv = argv + ["update", "bss:"]
+    args = apsbss.get_options()
+    assert args is not None
+    assert args.subcommand == sys.argv[1]
+    assert args.prefix == sys.argv[2]
