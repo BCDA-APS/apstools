6475035b1e5e20a431836f5793be66d03deefb7d
Pete R Jemian <prjemian@gmail.com>
2021-10-19 21:45:36 -0500

    DOC Kohzu DCM

--- apstools/_devices/tests/test_kohzu_monochromator.py
+++ apstools/_devices/tests/test_kohzu_monochromator.py
@@ -22,32 +22,37 @@ except RuntimeError:
 
 
 class MyKohzu(KohzuSeqCtl_Monochromator):
+    """
+    Kohzu DCM with controls to enable wavelength controls.
+
+    Wavelength controls are disabled when the physical motors are out of range
+    for the choice of wavelength (such as mathematical singularity for energy
+    when ``m_theta == 0` on start of the IOC.)
+    """
+
     m_theta = Component(EpicsMotor, "m45")
     m_y = Component(EpicsMotor, "m46")
     m_z = Component(EpicsMotor, "m47")
 
     def into_control_range(self, p_theta=2, p_y=-15, p_z=90):
         """
-        Move the Kohzu motors into range so the energy controls will work.
+        Move the Kohzu motors into range so the wavelength controls will work.
 
-        Written as a bluesky plan so that all motors can be moved simultaneously.
-        Return early if the motors are already in range.
+        Written as a bluesky plan so that all motors can be moved
+        simultaneously. Return early if the motors are already in range.
         """
-        if (
-            self.m_theta.position >= p_theta
-            and self.m_y.position <= p_y
-            and self.m_z.position >= p_z
-        ):
+        args = []
+        if self.m_theta.position < p_theta:
+            args += [self.m_theta, p_theta]
+        if self.m_y.position > p_y:
+            args += [self.m_y, p_y]
+        if self.m_z.position < p_z:
+            args += [self.m_z, p_z]
+        if (len(args) == 0):
             # all motors in range, no work to do, MUST yield something
             yield from bps.null()
             return
-        # fmt: off
-        yield from bps.mv(
-            self.m_theta, p_theta,
-            self.m_y, p_y,
-            self.m_z, p_z,
-        )
-        # fmt: on
+        yield from bps.mv(*args)
         yield from bps.sleep(1)  # allow IOC to react
         yield from bps.mv(self.operator_acknowledge, 1)
 
