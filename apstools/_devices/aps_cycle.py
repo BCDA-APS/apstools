09b9e34f148ddda60b9687ac81ea68cf533fe764
Pete R Jemian <prjemian@gmail.com>
2021-11-26 15:38:46 -0600

    MNT #570 isolate ModuleNotFoundError

--- apstools/_devices/aps_cycle.py
+++ apstools/_devices/aps_cycle.py
@@ -74,12 +74,12 @@ class _ApsCycleDB:
         APS Data Management package (*aps-dm-api*).  This allows
         automatic updates as needed.
         """
-        try:
-            table = self._bss_list_runs
-            self.source = "aps-dm-api"
-        except Exception:
+        table = self._bss_list_runs
+        if table is None:
             table = open(LOCAL_FILE, "r").read()
             self.source = "file"
+        else:
+            self.source = "aps-dm-api"
         return table
 
     def _write_cycle_data(self):
@@ -95,13 +95,18 @@ class _ApsCycleDB:
             from apstools._devices.aps_cycle import cycle_db
             cycle_db._write_cycle_data()
         """
-        open(LOCAL_FILE, "w").write(self._bss_list_runs)
+        runs = self._bss_list_runs
+        if runs is not None:
+            open(LOCAL_FILE, "w").write(self._bss_list_runs)
 
     @property
     def _bss_list_runs(self):
-        import apsbss
+        try:
+            import apsbss
 
-        return str(apsbss.api_bss.listRuns())
+            return str(apsbss.api_bss.listRuns())
+        except ModuleNotFoundError:
+            return None
 
 
 cycle_db = _ApsCycleDB()
