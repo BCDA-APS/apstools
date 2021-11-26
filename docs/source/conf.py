39d99160328862db035a9483bcb0104d5c8cd652
Pete R Jemian <prjemian@gmail.com>
2021-11-26 15:50:24 -0600

    #582 needs this extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,14 +32,15 @@ import apstools
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.todo',
-    'sphinx.ext.viewcode',
-    ]
+extensions = """
+    sphinx.ext.autodoc
+    sphinx.ext.autosummary
+    sphinx.ext.coverage
+    sphinx.ext.githubpages
+    sphinx.ext.mathjax
+    sphinx.ext.todo
+    sphinx.ext.viewcode
+""".split()
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
