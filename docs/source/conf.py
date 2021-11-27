22d46034ecdc932862bb4c235a5d412717a50bbf
Pete R Jemian <prjemian@gmail.com>
2021-11-26 18:39:42 -0600

    DOC revert to sphinx-rtd-theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -94,8 +94,8 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-# html_theme = 'sphinx_rtd_theme'
-html_theme = 'bizstyle'  # ok
+html_theme = 'sphinx_rtd_theme'
+# html_theme = 'bizstyle'  # ok
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
