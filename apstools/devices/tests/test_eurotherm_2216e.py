0d305875269bfa6f0a0d09a3254a4c27d5672704
Pete Jemian <prjemian@gmail.com>
2022-01-10 15:27:13 -0600

    TST #614

--- apstools/devices/tests/test_eurotherm_2216e.py
+++ apstools/devices/tests/test_eurotherm_2216e.py
@@ -11,15 +11,18 @@ PV_PREFIX = f"phony:{IOC}2216e:"
 
 
 def test_device():
-    controller = eurotherm_2216e.Eurotherm2216e(
-        PV_PREFIX, name="controller", readback_pv="rb", setpoint_pv="w"
-    )
-    assert not controller.connected
-    assert controller.tolerance.get() == 1
+    euro = eurotherm_2216e.Eurotherm2216e(PV_PREFIX, name="controller")
+    assert not euro.connected
+    assert euro.tolerance.get() == 1
 
     cns = """
-    readback setpoint sensor
-    done target tolerance report_dmov_changes
+    readback setpoint
+    """.split()
+    assert sorted(euro.read_attrs) == sorted(cns)
+
+    cns += """
+    sensor
+    done tolerance report_dmov_changes
     mode power program_number
     """.split()
-    assert sorted(controller.component_names) == sorted(cns)
+    assert sorted(euro.component_names) == sorted(cns)
