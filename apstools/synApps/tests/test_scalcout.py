2d0b7c22f29aaad56b03f03e234b4e61ed413198
Pete R Jemian <prjemian@gmail.com>
2022-01-19 17:00:11 -0600

    TST #607 scalcout

--- apstools/synApps/tests/test_scalcout.py
+++ apstools/synApps/tests/test_scalcout.py
@@ -1,5 +1,7 @@
 import time
 
+from apstools.synApps.tests.test_luascript import SHORT_DELAY_FOR_EPICS
+
 from ..scalcout import ScalcoutRecord
 from ..scalcout import UserScalcoutDevice
 
@@ -25,13 +27,32 @@ def test_scalcout_reset():
 
     calc = user.scalcout10
     assert isinstance(calc, ScalcoutRecord)
-    calc.enable.put("E")  # Note: only "E"
 
-    # TODO: set some things
+    calc.reset()
+    assert calc.enable.get() in [1, "E"]
+
+    # set some things
+    calc.description.put("unit testing")
+    v = 1.23456
+    calc.channels.A.input_value.put(v)
+    calc.channels.BB.input_value.put("testing")
+    calc.calculation.put("A")
+    calc.process_record.put(1)
+    time.sleep(SHORT_DELAY_FOR_EPICS)
+    assert calc.channels.A.input_value.get() == v
+    assert calc.channels.BB.input_value.get() == "testing"
+    assert calc.calculation.get() == "A"
+    assert round(calc.calculated_value.get(), 5) == v
+    assert calc.calculated_value_string.get() == str(v)
 
     calc.reset()
     assert calc.description.get() == calc.prefix
     assert calc.calculation.get() == "0"
+    assert calc.channels.A.input_value.get() == 0
+    assert calc.channels.BB.input_value.get() == ""
+    assert round(calc.calculated_value.get(), 5) == 0
+    assert calc.calculated_value_string.get() == "0.00000"
+
     v1 = calc.calculated_value.get()
     time.sleep(0.2)
     assert v1 == calc.calculated_value.get()
