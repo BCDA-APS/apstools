1c916981114f613c6d5897941389c539dfd4c67b
Pete R Jemian <prjemian@gmail.com>
2022-05-31 01:20:13 -0500

    TST #655 test  the unexpected key

---
+++ apstools/devices/tests/test_issue655.py
@@ -0,0 +1,116 @@
+from ..area_detector_support import AD_EpicsHdf5FileName
+from bluesky import plans as bp
+from bluesky import RunEngine
+from ophyd.areadetector import ADComponent, SimDetectorCam
+from ophyd.areadetector import SimDetector, SingleTrigger
+from ophyd.areadetector.filestore_mixins import FileStoreIterativeWrite
+from ophyd.areadetector.plugins import HDF5Plugin_V34 as HDF5Plugin
+from ophyd.areadetector.plugins import ImagePlugin_V34 as ImagePlugin
+import databroker
+import pathlib
+
+
+IOC = "ad:"
+IMAGE_DIR = "images"
+AD_IOC_MOUNT_PATH = pathlib.Path("/tmp")
+BLUESKY_MOUNT_PATH = pathlib.Path(
+    f"/tmp/docker_ioc/ioc{IOC.rstrip(':')}{AD_IOC_MOUNT_PATH}"
+)
+
+# MUST end with a `/`, pathlib will NOT provide it
+WRITE_PATH_TEMPLATE = f"{AD_IOC_MOUNT_PATH / IMAGE_DIR}/"
+READ_PATH_TEMPLATE = f"{BLUESKY_MOUNT_PATH / IMAGE_DIR}/"
+
+
+class myHdf5EpicsIterativeWriter(AD_EpicsHdf5FileName, FileStoreIterativeWrite):
+    """Build a custom iterative writer using the override."""
+
+    pass
+
+
+class myHDF5FileNames(HDF5Plugin, myHdf5EpicsIterativeWriter):
+    """Make a custom HDF5 plugin using the override."""
+
+    pass
+
+
+# define support for the detector (ADSimDetector here):
+class MySimDetector(SingleTrigger, SimDetector):
+    """SimDetector with HDF5 file names specified by EPICS."""
+
+    _default_read_attrs = ["hdf1"]
+
+    cam = ADComponent(SimDetectorCam, "cam1:")
+    image = ADComponent(ImagePlugin, "image1:")
+
+    hdf1 = ADComponent(
+        myHDF5FileNames,
+        suffix="HDF1:",
+        write_path_template=WRITE_PATH_TEMPLATE,
+        read_path_template=READ_PATH_TEMPLATE,
+    )
+
+
+def test_no_unexpected_key_in_datum_kwarg():
+    """
+    Test that this exception is fixed:
+
+    TypeError: __call__() got an unexpected keyword argument 'HDF5_file_name'
+    """
+    cat = databroker.temp().v2
+    RE = RunEngine({})
+    RE.subscribe(cat.v1.insert)
+
+    camera = MySimDetector(IOC, name="camera")
+    assert isinstance(camera, MySimDetector)
+
+    if hasattr(camera.hdf1.stage_sigs, "array_counter"):
+        # remove this so array counter is not set to zero each staging
+        del camera.hdf1.stage_sigs["array_counter"]
+
+    camera.cam.stage_sigs["num_images"] = 1
+    camera.hdf1.stage_sigs["num_capture"] = 1
+    camera.hdf1.stage_sigs["file_template"] = "%s%s_%3.3d.h5"
+    camera.hdf1.stage_sigs["compression"] = "zlib"
+
+    # ensure the capture is always set last
+    camera.hdf1.stage_sigs["capture"] = camera.hdf1.stage_sigs.pop("capture")
+
+    camera.wait_for_connection()
+    assert camera.connected
+
+    camera.hdf1.warmup()
+
+    file_path = AD_IOC_MOUNT_PATH / IMAGE_DIR
+    camera.hdf1.create_directory.put(-5)
+    camera.hdf1.file_name.put("test_image")
+    camera.hdf1.file_path.put(str(file_path))
+    camera.hdf1.stage_sigs["file_template"] = "%s%s_%4.4d.h5"
+
+    # take the image
+    uids = RE(bp.count([camera], num=1))
+    assert len(uids) == 1
+
+    uid = uids[0]
+    assert isinstance(uid, str)
+
+    # verify that the problem key in datum_kwargs is not found
+    for uid in uids:
+        for doc_type, doc in cat.v1[uid].documents():
+            if doc_type == "datum":
+                datum_kwargs = doc.get("datum_kwargs")
+                assert datum_kwargs is not None
+
+                # this key is expected
+                assert "point_number" in datum_kwargs
+
+                # this is the unexpected key
+                assert "HDF5_file_name" not in datum_kwargs
+
+    run = cat.v2[uid]
+    assert run is not None
+
+    # If this errors, that's issue 655:
+    ds = run.primary.read()
+    assert ds is not None
+    assert camera.image.name in ds
