bee0ac161b662c3455490ef4e78db002647d53c2
Pete R Jemian <prjemian@gmail.com>
2022-07-06 00:54:39 -0500

    ENH #652 add nbsphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,6 +40,7 @@ extensions = """
     sphinx.ext.mathjax
     sphinx.ext.todo
     sphinx.ext.viewcode
+    nbsphinx
 """.split()
 
 # Add any paths that contain templates here, relative to this directory.
