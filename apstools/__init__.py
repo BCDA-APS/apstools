a467bc1085c63532cce2b8b1917fc472c0bba12d
Pete Jemian <prjemian@gmail.com>
2022-09-20 07:10:55 -0500

    PKG #702 PEP518

--- apstools/__init__.py
+++ apstools/__init__.py
@@ -1,64 +1,19 @@
+try:
+    from setuptools_scm import get_version
+
+    __version__ = get_version()
+    del get_version
+except (LookupError, ModuleNotFoundError):
+    from importlib.metadata import version
+
+    __version__ = version("apstools")
+
 # -----------------------------------------------------------------------------
 # :author:    Pete R. Jemian
-# :email:     jemian@anl.gov
-# :copyright: (c) 2017-2022, UChicago Argonne, LLC
+# :email:     prjemian@gmail.com
+# :copyright: (c) 2014-2022, Pete R. Jemian
 #
 # Distributed under the terms of the Creative Commons Attribution 4.0 International Public License.
 #
-# The full license is in the file LICENSE.txt, distributed with this software.
+# The full license is in the file LICENSE, distributed with this software.
 # -----------------------------------------------------------------------------
-
-__project__ = u"apstools"
-__description__ = (
-    u"Various Python tools for use with the Bluesky Framework at the APS"
-)
-__copyright__ = u"2017-2022, UChicago Argonne, LLC"
-__authors__ = [
-    u"Pete Jemian",
-]
-__author__ = ", ".join(__authors__)
-__institution__ = u"Advanced Photon Source, Argonne National Laboratory"
-__author_email__ = u"jemian@anl.gov"
-__url__ = u"https://bcda-aps.github.io/apstools/"
-__license__ = u"(c) " + __copyright__
-__license__ += u" (see LICENSE.txt file for details)"
-__platforms__ = "any"
-__zip_safe__ = False
-__exclude_project_dirs__ = "docs resources".split()
-__python_version_required__ = ">=3.8, <3.11"
-
-__package_name__ = __project__
-__long_description__ = __description__
-
-from ._requirements import (
-    learn_requirements,
-)  # lgtm [py/import-own-module]
-
-__install_requires__ = learn_requirements()
-del learn_requirements
-
-__classifiers__ = [
-    "Development Status :: 5 - Production/Stable",
-    "Environment :: Console",
-    "Intended Audience :: Science/Research",
-    "License :: Freely Distributable",
-    "License :: Public Domain",
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.8",
-    "Programming Language :: Python :: 3.9",
-    "Programming Language :: Python :: 3.10",
-    "Topic :: Scientific/Engineering",
-    "Topic :: Scientific/Engineering :: Astronomy",
-    "Topic :: Scientific/Engineering :: Bio-Informatics",
-    "Topic :: Scientific/Engineering :: Chemistry",
-    "Topic :: Scientific/Engineering :: Information Analysis",
-    "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator",
-    "Topic :: Scientific/Engineering :: Mathematics",
-    "Topic :: Scientific/Engineering :: Physics",
-    "Topic :: Software Development :: Embedded Systems",
-    "Topic :: Software Development :: Libraries :: Python Modules",
-    "Topic :: Scientific/Engineering :: Visualization",
-    "Topic :: Software Development",
-    "Topic :: Utilities",
-]
