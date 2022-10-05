f5584d256ad0642a1ff09973e44e6beddb7fadf3
Pete R Jemian <prjemian@gmail.com>
2022-10-05 11:52:30 -0500

    TST #725 done moving?

--- apstools/devices/tests/test_positioner_soft_done.py
+++ apstools/devices/tests/test_positioner_soft_done.py
@@ -379,15 +379,18 @@ def test_same_position_725(target, calcpos):
     if calcpos.position == target:
         # First, move away from the target.
         status = calcpos.move(-target or 0.1 * (1 + random.random()))
+        assert status.done
         assert status.elapsed > 0, str(status)
         confirm_in_position(calcpos)
 
     # Move to the target position.
     status = calcpos.move(target)
+    assert status.done
     assert status.elapsed > 0, str(status)
     confirm_in_position(calcpos)
 
     # Do it again (the reason for issue #725).
     status = calcpos.move(target)
+    assert status.done
     assert status.elapsed > 0, str(status)
     confirm_in_position(calcpos)
