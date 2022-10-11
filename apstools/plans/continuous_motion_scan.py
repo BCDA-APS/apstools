119bd537825b27757b4a797f34d25b2a6d7be142
Pete R Jemian <prjemian@gmail.com>
2022-10-11 10:19:25 -0500

    MNT #646 flyer backbone

---
+++ apstools/plans/continuous_motion_scan.py
@@ -0,0 +1,71 @@
+"""
+Continuous motion scan using scaler v. motor
+++++++++++++++++++++++++++++++++++++++++++++
+
+.. autosummary::
+
+   ~ContinuousScalerMotorFlyer
+   ~fly_scaler_motor
+
+New in release 1.6.6
+"""
+
+from bluesky import plans as bp
+from ophyd import Device
+from ophyd import DeviceStatus
+import time
+
+
+def fly_scaler_motor(
+    # scaler, motor, start_position, end_position,
+    # velocity=None, num_points=10,
+    md={}
+):
+    """
+    Run the ContinuousScalerMotorFlyer in a plan.
+
+    ::
+
+        RE(fly_scaler_motor(scaler, motor, start_position, end_position, velocity=None, num_points=10, md={}))
+
+    New in release 1.6.6
+    """
+    flyer = ContinuousScalerMotorFlyer(
+        # scaler, motor, start_position, end_position,
+        # velocity=velocity, num_points=num_points,
+        name="fly_scaler_motor_object"
+    )
+    if md is None:
+        md = {}
+    yield from bp.fly([flyer], md=md)
+
+
+class ContinuousScalerMotorFlyer(Device):
+    """
+    Documentation
+
+    New in release 1.6.6
+    """
+    # TODO: move to devices
+
+    def kickoff(self):
+        """Start the flyer."""
+        status = DeviceStatus(self)
+        status.set_finished()  # ALWAYS in kickoff()
+        return status
+
+    def complete(self):
+        """Wait for the flyer to complete."""
+        status = DeviceStatus(self)
+        status.set_finished()  # once the flyer is truly complete
+        return status
+
+    def describe_collect(self):
+        """Describe the data from collect()."""
+        schema = {}
+        return {"primary": schema}
+
+    def collect(self):
+        """Report data collected by this flyer."""
+        event = dict(time=time.time(), data={}, timestamps={})
+        yield event
