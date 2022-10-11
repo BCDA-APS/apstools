000b2d2d74a15e49cb451284ba44c7a89dc5de8f
Pete R Jemian <prjemian@gmail.com>
2022-10-11 10:58:23 -0500

    ENH #646 add the API

--- apstools/plans/continuous_motion_scan.py
+++ apstools/plans/continuous_motion_scan.py
@@ -16,24 +16,30 @@ from ophyd import DeviceStatus
 import time
 
 
-def fly_scaler_motor(
-    # scaler, motor, start_position, end_position,
-    # velocity=None, num_points=10,
-    md={}
-):
+def fly_scaler_motor(scaler, motor, start_position, end_position, velocity=None, num_points=10, md={}):
     """
     Run the ContinuousScalerMotorFlyer in a plan.
 
     ::
 
-        RE(fly_scaler_motor(scaler, motor, start_position, end_position, velocity=None, num_points=10, md={}))
+        RE(
+            fly_scaler_motor(
+                scaler, motor, start_position, end_position,
+                velocity=None, num_points=10,
+                md={},
+            )
+        )
 
     New in release 1.6.6
     """
     flyer = ContinuousScalerMotorFlyer(
-        # scaler, motor, start_position, end_position,
-        # velocity=velocity, num_points=num_points,
-        name="fly_scaler_motor_object"
+        scaler,
+        motor,
+        start_position,
+        end_position,
+        velocity=velocity,
+        num_points=num_points,
+        name="fly_scaler_motor_object",
     )
     if md is None:
         md = {}
@@ -46,7 +52,40 @@ class ContinuousScalerMotorFlyer(Device):
 
     New in release 1.6.6
     """
+
     # TODO: move to devices
+
+    def __init__(
+        self,
+        scaler,
+        motor,
+        start_position,
+        end_position,
+        *args,
+        velocity=None,
+        num_points=10,
+        stream_name="primary",
+        # md={},
+        **kwargs,
+    ):
+        if not hasattr(scaler, "count"):
+            raise TypeError(f"Not prepared to handle object as scaler: {scaler=}")
+        if not hasattr(motor, "motor_done_move"):
+            raise TypeError(f"Not prepared to handle object as motor: {motor=}")
+
+        self._scaler = scaler
+        self._motor = motor
+        self._start_position = start_position
+        self._end_position = end_position
+        self._velocity = velocity
+        self._num_points = num_points
+
+        # self._original = {}
+        # self._events = []
+
+        self.stream_name = stream_name
+
+        super().__init__(*args, **kwargs)
 
     def kickoff(self):
         """Start the flyer."""
@@ -63,7 +102,7 @@ class ContinuousScalerMotorFlyer(Device):
     def describe_collect(self):
         """Describe the data from collect()."""
         schema = {}
-        return {"primary": schema}
+        return {self.stream_name: schema}
 
     def collect(self):
         """Report data collected by this flyer."""
--- apstools/plans/tests/test_continuous_motion_scan.py
+++ apstools/plans/tests/test_continuous_motion_scan.py
@@ -1,10 +1,11 @@
 from .. import ContinuousScalerMotorFlyer
 from .. import fly_scaler_motor
+from ...tests import IOC
 from bluesky import plans as bp
 from bluesky import RunEngine
 import databroker
+import ophyd
 import pytest
-from ophyd import DeviceStatus
 import time
 
 
@@ -12,28 +13,36 @@ cat = databroker.temp()
 RE = RunEngine({})
 RE.subscribe(cat.v1.insert)
 
+m1 = ophyd.EpicsMotor(f"{IOC}m1", name="m1")
+scaler1 = ophyd.scaler.ScalerCH(f"{IOC}scaler1", name="scaler1")
+for obj in (m1, scaler1):
+    obj.wait_for_connection()
 
-def test_kickoff():
-    flyer = ContinuousScalerMotorFlyer(name="flyer")
+
+@pytest.fixture(scope="function")
+def flyer():
+    obj = ContinuousScalerMotorFlyer(scaler1, m1, -1, 1, name="flyer")
+    return obj
+
+
+def test_kickoff(flyer):
     assert flyer is not None
 
     st = flyer.kickoff()
     assert not st.done
-    assert isinstance(st, DeviceStatus)
+    assert isinstance(st, ophyd.DeviceStatus)
 
     time.sleep(0.000_001)
     assert st.done
 
 
-def test_describe_collect():
-    flyer = ContinuousScalerMotorFlyer(name="flyer")
+def test_describe_collect(flyer):
     flyer.kickoff()
     schema = flyer.describe_collect()
     assert isinstance(schema, dict)
 
 
-def test_collect():
-    flyer = ContinuousScalerMotorFlyer(name="flyer")
+def test_collect(flyer):
     flyer.kickoff()
     event = flyer.collect()
     assert not isinstance(event, dict)
@@ -46,15 +55,12 @@ def test_collect():
     assert "timestamps" in events[0]
 
 
-@pytest.mark.parametrize(
-    "method", ["device", "plan"]
-)
-def test_flyer(method):
+@pytest.mark.parametrize("method", ["device", "plan"])
+def test_flyer(method, flyer):
     if method == "device":
-        flyer = ContinuousScalerMotorFlyer(name="flyer")
         uids = RE(bp.fly([flyer]))
     elif method == "plan":
-        uids = RE(fly_scaler_motor())
+        uids = RE(fly_scaler_motor(scaler1, m1, -1, 1))
     else:
         raise ValueError(f"Unknown {method=}.")
     assert len(uids) == 1
