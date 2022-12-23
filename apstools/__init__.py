0e66a7b0ebaf8ccd4e8069883ded2cc4907eb4fe
Pete R Jemian <prjemian@gmail.com>
2022-12-22 21:29:51 -0600

    MNT #771

--- apstools/__init__.py
+++ apstools/__init__.py
@@ -1,12 +1,17 @@
+# -*- coding: iso-8859-1 -*-
+
+"""Configuration of apstools package."""
+
 try:
     from setuptools_scm import get_version
 
-    __version__ = get_version()
+    __version__ = get_version(root="..", relative_to=__file__)
     del get_version
 except (LookupError, ModuleNotFoundError):
     from importlib.metadata import version
 
     __version__ = version("apstools")
+    del version
 
 # -----------------------------------------------------------------------------
 # :author:    Pete R. Jemian
