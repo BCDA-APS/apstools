f3919f5e5821a228a02a4e5780efbc1501751ce0
Pete R Jemian <prjemian@gmail.com>
2023-01-30 14:24:11 -0600

    TST #748 call cb_readback()

--- apstools/devices/tests/test_positioner_soft_done.py
+++ apstools/devices/tests/test_positioner_soft_done.py
@@ -97,7 +97,10 @@ def confirm_in_position(p):
     rb = p.readback.get(use_monitor=False)
     sp = p.setpoint.get(use_monitor=False)
     tol = p.actual_tolerance
+    assert p.inposition, f"{rb=}  {sp=}   {tol=}"
     assert math.isclose(rb, sp, abs_tol=tol), f"{rb=}  {sp=}   {tol=}"
+    p.cb_readback()
+    assert p.done.get() == p.done_value, f"{rb=}  {sp=}   {tol=}"
 
 
 @run_in_thread
