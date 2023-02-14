7cfb735138683801c87f9f194003857a300e3d7b
Pete R Jemian <prjemian@gmail.com>
2023-02-14 10:28:50 -0600

    TST #798 revise for tempdir use

--- apstools/callbacks/tests/test_440_specwriter.py
+++ apstools/callbacks/tests/test_440_specwriter.py
@@ -11,12 +11,15 @@ import zipfile
 import intake
 import numpy as np
 import pytest
+import yaml
 
 from .. import SpecWriterCallback
 from ..spec_file_writer import _rebuild_scan_command
 
+CAT_NAME = "packed_catalog"
 DATA_ARCHIVE = "440_specwriter_problem_run"
 FULL_ZIP_FILE = pathlib.Path(__file__).parent / f"{DATA_ARCHIVE}.zip"
+UID = "624e776a-a914-4a74-8841-babf1591fb29"
 
 
 @pytest.fixture(scope="function")
@@ -36,8 +39,19 @@ def catalog():
         zip_ref.extractall(str(tempdir))
 
     catalog = tempdir / DATA_ARCHIVE / "catalog.yml"
-    assert catalog.parent.exists(), f"{catalog.parent=}"
+    cat_dir = catalog.parent
+    assert cat_dir.exists(), f"{catalog.parent=}"
     assert catalog.exists(), f"{catalog=}"
+    assert (cat_dir / "documents_manifest.txt").exists()
+    assert (cat_dir / "documents").exists()
+    assert (cat_dir / "documents" / f"{UID}.msgpack").exists()
+
+    # edit the catalog file for the installed path
+    details = yaml.load(open(catalog, "r").read(), Loader=yaml.Loader)
+    args = details["sources"][CAT_NAME]["args"]
+    args["paths"] = [args["paths"][0].replace("/tmp/", f"{tempdir}/")]
+    with open(catalog, "w") as f:
+        f.write(yaml.dump(details, indent=2))
 
     yield catalog
 
@@ -45,25 +59,30 @@ def catalog():
 def test_confirm_run_exists(catalog):
     cat = intake.open_catalog(catalog)
     assert len(cat) > 0, f"{catalog=}   {cat=}"
-    assert "packed_catalog" in cat, f"{catalog=}   {cat=}"
-
-    cat = cat["packed_catalog"]
+    assert CAT_NAME in cat, f"{catalog=}   {cat=}"
     assert isinstance(cat, intake.Catalog), f"{type(cat)=}  {dir(cat)=}"
     assert isinstance(cat, intake.catalog.Catalog), f"{type(cat)=}  {dir(cat)=}"
     assert isinstance(cat, intake.catalog.local.Catalog), f"{type(cat)=}  {dir(cat)=}"
-    # locally, next test passes, in CI, it is databroker._drivers.msgpack.BlueskyMsgpackCatalog
-    # assert isinstance(cat, intake.catalog.local.YAMLFileCatalog), f"{type(cat)=}  {dir(cat)=}"
-    # assert False, f"{type(cat)=}  {dir(cat)=}"
-    # assert cat.path == str(catalog), f"{dir(cat)=}"  "path" exists only for YAMLFileCatalog
+    assert isinstance(cat, intake.catalog.local.YAMLFileCatalog), f"{type(cat)=}  {dir(cat)=}"
     assert cat.name == DATA_ARCHIVE
-    assert len(cat) == 1, f"{catalog=}  {cat=}  {dir(cat)=}"
-    assert "624e776a-a914-4a74-8841-babf1591fb29" in cat, f"{catalog=}  {cat=}  {dir(cat)=}"
+
+    cat = cat[CAT_NAME]
+    assert isinstance(cat, intake.Catalog), f"{type(cat)=}  {dir(cat)=}"
+    assert isinstance(cat, intake.catalog.Catalog), f"{type(cat)=}  {dir(cat)=}"
+    assert isinstance(cat, intake.catalog.local.Catalog), f"{type(cat)=}  {dir(cat)=}"
+    assert not isinstance(cat, intake.catalog.local.YAMLFileCatalog), f"{type(cat)=}  {dir(cat)=}"
+    assert "msgpack.BlueskyMsgpackCatalog" in str(type(cat))
+    assert cat.name == CAT_NAME
+    assert len(cat) > 0, f"{catalog=}  {cat=}  {dir(cat)=}"
+
+    # finally, confirm the expected run exists
+    assert UID in cat, f"{catalog=}  {cat=}  {dir(cat)=}"
 
 
 def test_specwriter_replay(tempdir, catalog):
     # https://github.com/BCDA-APS/apstools/issues/440
-    # The problem does not appear when using data from the databroker.
-    # Verify that is the case now.
+    # The #440 problem does not appear when using data from the databroker.
+    # This test will verify that is still the case now.
     pathlib.os.chdir(tempdir)
     specfile = pathlib.Path("issue240.spec")
     if specfile.exists():
@@ -71,7 +90,7 @@ def test_specwriter_replay(tempdir, catalog):
     specwriter = SpecWriterCallback()
     specwriter.newfile(specfile)
 
-    cat = intake.open_catalog(catalog)["packed_catalog"].v2
+    cat = intake.open_catalog(catalog)[CAT_NAME].v2
     assert len(cat) > 0, f"{catalog=}   {cat=}"
     h = cat.v1[-1]
     for key, doc in cat.v1.get_documents(h):
@@ -92,14 +111,14 @@ def test_specwriter_replay(tempdir, catalog):
 
 
 def test_specwriter_numpy_array(tempdir, catalog):
-    # The problem comes up if one of the arguments is a numpy.array.
+    # The #440 problem comes up if one of the arguments is a numpy.array.
     # So we must replay the document stream and modify the right
     # structure as it passes by.
     # This structure is in the start document, which is first.
     # Note: we don't have to write the whole SPEC file again,
     # just test if _rebuild_scan_command(start_doc) is one line.
 
-    cat = intake.open_catalog(catalog)["packed_catalog"].v2
+    cat = intake.open_catalog(catalog)[CAT_NAME].v2
     assert len(cat) > 0, f"{catalog=}   {cat=}"
 
     hh = cat.v1.get_documents(cat.v1[-1])
@@ -117,4 +136,4 @@ def test_specwriter_numpy_array(tempdir, catalog):
     # modify the start doc
     doc["plan_args"]["qx_setup"]["relative_energy"] = arr
     cmd = _rebuild_scan_command(doc)  # FIXME: <-----
-    assert len(cmd.strip().splitlines()) == 1
+    assert len(cmd.strip().splitlines()) == 1, f"{cmd=}"
