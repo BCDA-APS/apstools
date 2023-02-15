0d5876f80778965ff3f7fbd1411244eeee0422f9
Pete R Jemian <prjemian@gmail.com>
2023-02-15 10:09:19 -0600

    TST #799 create new tests

---
+++ apstools/devices/tests/test_Struck3820.py
@@ -0,0 +1,43 @@
+import pytest
+
+from ..struck3820 import Struck3820
+
+COMPONENT_NAMES = """
+    start_all 
+    stop_all
+    erase_start
+    erase_all 
+    mca1 
+    mca2 
+    mca3 
+    mca4 
+    clock_frequency 
+    current_channel 
+    channel_max 
+    channels_used 
+    elapsed_real_time 
+    preset_real_time 
+    dwell_time  
+    prescale 
+    acquiring  
+    acquire_mode
+    model  
+    firmware
+    channel_advance 
+    count_on_start 
+    software_channel_advance
+    channel1_source  
+    user_led  
+    mux_output 
+    input_mode 
+    output_mode
+    output_polarity
+    read_rate  
+    do_read_all 
+""".split()
+
+
+@pytest.mark.parametrize("name", COMPONENT_NAMES)
+def test_component_names_exist(name):
+    device = Struck3820("phony:epics:name:", name="device")
+    assert name in device.component_names
