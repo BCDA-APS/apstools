48045fe89b487c94b854766c8799d418f42fc76b
Mark Wolfman <canismarko@gmail.com>
2024-01-12 12:45:46 -0600

    Added pre-determined settling times to SRS570 pre-amp gain signals.

--- apstools/devices/srs570_preamplifier.py
+++ apstools/devices/srs570_preamplifier.py
@@ -21,10 +21,162 @@ import logging
 import pint
 from ophyd import Component
 from ophyd import EpicsSignal
+from ophyd.signal import DEFAULT_WRITE_TIMEOUT
 
 from .preamp_base import PreamplifierBaseDevice
 
 logger = logging.getLogger(__name__)
+
+
+gain_units = ["pA/V", "nA/V", "uA/V", "mA/V"]
+gain_values = ["1", "2", "5", "10", "20", "50", "100", "200", "500"]
+gain_modes = ["LOW NOISE", "HIGH BW"]
+
+# Settling times measured from the 25-ID-C upstream I0 chamber's SR-570
+# (sensitivity_value, sensitivity_unit, gain_mode): settle_time
+settling_times = {
+    # pA/V
+    ("1", "pA/V", "HIGH BW"): 2,
+    ("2", "pA/V", "HIGH BW"): 1.75,
+    ("5", "pA/V", "HIGH BW"): 1.5,
+    ("10", "pA/V", "HIGH BW"): 0.75,
+    ("20", "pA/V", "HIGH BW"): 0.3,
+    ("50", "pA/V", "HIGH BW"): 0.3,
+    ("100", "pA/V", "HIGH BW"): 0.3,
+    ("200", "pA/V", "HIGH BW"): 0.2,
+    ("500", "pA/V", "HIGH BW"): 0.2,
+    ("1", "pA/V", "LOW NOISE"): 2.0,
+    ("2", "pA/V", "LOW NOISE"): 1.75,
+    ("5", "pA/V", "LOW NOISE"): 1.5,
+    ("10", "pA/V", "LOW NOISE"): 1.25,
+    ("20", "pA/V", "LOW NOISE"): 1.0,
+    ("50", "pA/V", "LOW NOISE"): 1.0,
+    ("100", "pA/V", "LOW NOISE"): 1.0,
+    ("200", "pA/V", "LOW NOISE"): 0.3,
+    ("500", "pA/V", "LOW NOISE"): 0.3,
+}
+settling_times.update(
+    {
+        # nA/V
+        (gain_values[idx], "nA/V", "HIGH BW"): 0.2
+        for idx in range(9)
+    }
+)
+settling_times.update(
+    {
+        # nA/V
+        ("1", "nA/V", "LOW NOISE"): 0.3,
+        ("2", "nA/V", "LOW NOISE"): 0.2,
+        ("5", "nA/V", "LOW NOISE"): 0.2,
+        ("10", "nA/V", "LOW NOISE"): 0.2,
+        ("20", "nA/V", "LOW NOISE"): 0.2,
+        ("50", "nA/V", "LOW NOISE"): 0.2,
+        ("100", "nA/V", "LOW NOISE"): 0.2,
+        ("200", "nA/V", "LOW NOISE"): 0.2,
+        ("500", "nA/V", "LOW NOISE"): 0.2,
+    }
+)
+settling_times.update(
+    {
+        # μA/V, high bandwidth
+        (gain_values[idx], "uA/V", "HIGH BW"): 0.15
+        for idx in range(9)
+    }
+)
+settling_times.update(
+    {
+        # μA/V, low noise
+        (gain_values[idx], "uA/V", "LOW NOISE"): 0.15
+        for idx in range(9)
+    }
+)
+settling_times.update(
+    {
+        ("1", "mA/V", "HIGH BW"): 0.15,
+        ("1", "mA/V", "LOW NOISE"): 0.15,
+    }
+)
+
+
+class GainSignal(EpicsSignal):
+    """A signal where the settling time depends on the pre-amp gain.
+
+    Used to introduce a specific settle time when setting to account
+    for the amp's R–C relaxation time when changing gain.
+
+    """
+    @staticmethod
+    def _settle_time(gain_value: int, gain_unit: int, gain_mode: str):
+        """Determine the best settle time for a given combination of parameters.
+
+        Parameters can be strings of indexes.
+
+        """
+        # Convert indexes to string values
+        try:
+            gain_value = gain_values[gain_value]
+        except (TypeError, IndexError):
+            pass
+        try:
+            gain_unit = gain_units[gain_unit]
+        except (TypeError, IndexError):
+            pass
+        try:
+            gain_mode = gain_modes[gain_mode]
+        except (TypeError, IndexError):
+            pass
+        # Get calibrated settle time, or None to use the Ophyd default
+        return settling_times.get((gain_value, gain_unit, gain_mode))
+
+    def set(self, value, *, timeout=DEFAULT_WRITE_TIMEOUT, settle_time="auto"):
+        """Set the value of the Signal and return a Status object.
+
+        If put completion is used for this EpicsSignal, the status object will
+        complete once EPICS reports the put has completed.
+
+        Otherwise the readback will be polled until equal to the set point (as
+        in `Signal.set`)
+
+        Parameters
+        ----------
+        value : any
+        timeout : float, optional
+            Maximum time to wait.
+        settle_time: float, optional
+            Delay after the set() has completed to indicate completion
+            to the caller. If "auto" (default), a reasonable settle
+            time will be chosen based on the gain mode on the pre-amp.
+
+        Returns
+        -------
+        st : Status
+
+        See Also
+        --------
+        Signal.set
+        EpicsSignal.set
+
+        """
+        # Determine optimal settling time.
+        if settle_time == "auto":
+            signals = [self.parent.sensitivity_value, self.parent.sensitivity_unit, self.parent.gain_mode]
+            args = [value if self is sig else sig.get() for sig in signals]
+            val, unit, mode = args
+            # Resolve string values to indices if provided
+            if val in gain_values:
+                val = gain_values.index(val)
+            if unit in gain_units:
+                unit = gain_units.index(unit)
+            if mode in gain_modes:
+                mode = gain_modes.index(mode)
+            # Low-drift mode uses the same settling times as low-noise mode
+            if mode == "LOW DRIFT":
+                mode = "LOW NOISE"
+            # Calculate settling time
+            _settle_time = self._settle_time(gain_value=val, gain_unit=unit, gain_mode=mode)
+        else:
+            _settle_time = settle_time
+        return super().set(value, timeout=timeout, settle_time=_settle_time)
 
 
 class SRS570_PreAmplifier(PreamplifierBaseDevice):
@@ -36,8 +188,8 @@ class SRS570_PreAmplifier(PreamplifierBaseDevice):
     # in the EPICS .db file.  Must cast them to ``float()`` or ``int()``
     # as desired.
     # see: https://github.com/epics-modules/ip/blob/master/ipApp/Db/SR570.db
-    sensitivity_value = Component(EpicsSignal, "sens_num", kind="config", string=True)
-    sensitivity_unit = Component(EpicsSignal, "sens_unit", kind="config", string=True)
+    sensitivity_value = Component(GainSignal, "sens_num", kind="config", string=True)
+    sensitivity_unit = Component(GainSignal, "sens_unit", kind="config", string=True)
 
     offset_on = Component(EpicsSignal, "offset_on", kind="config", string=True)
     offset_sign = Component(EpicsSignal, "offset_sign", kind="config", string=True)
@@ -46,7 +198,7 @@ class SRS570_PreAmplifier(PreamplifierBaseDevice):
     offset_fine = Component(EpicsSignal, "off_u_put", kind="config", string=True)
     offset_cal = Component(EpicsSignal, "offset_cal", kind="config", string=True)
 
-    set_all = Component(EpicsSignal, "init.PROC", kind="config")
+    set_all = Component(GainSignal, "init.PROC", kind="config")
 
     bias_value = Component(EpicsSignal, "bias_put", kind="config", string=True)
     bias_on = Component(EpicsSignal, "bias_on", kind="config", string=True)
@@ -55,7 +207,7 @@ class SRS570_PreAmplifier(PreamplifierBaseDevice):
     filter_lowpass = Component(EpicsSignal, "low_freq", kind="config", string=True)
     filter_highpass = Component(EpicsSignal, "high_freq", kind="config", string=True)
 
-    gain_mode = Component(EpicsSignal, "gain_mode", kind="config", string=True)
+    gain_mode = Component(GainSignal, "gain_mode", kind="config", string=True)
     invert = Component(EpicsSignal, "invert_on", kind="config", string=True)
     blank = Component(EpicsSignal, "blank_on", kind="config", string=True)
 
