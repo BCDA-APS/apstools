181808e399ea217cdc881af8c37ac090fab6b0c3
Pete Jemian <prjemian@gmail.com>
2024-07-01 16:39:01 -0500

    MNT #973 add revolver, refactor mixin classes

--- apstools/devices/aps_undulator.py
+++ apstools/devices/aps_undulator.py
@@ -5,6 +5,7 @@ APS undulators
 .. autosummary::
 
    ~PlanarUndulator
+   ~RevolverInsertionDevice
 """
 
 import logging
@@ -39,34 +40,30 @@ class UndulatorPositioner(PVPositioner):
     done_value = POSITIONER_DONE
 
 
-class PlanarUndulator(Device):
-    """APS Planar Undulator.
+class ID_Controls_Mixin(Device):
+    """Common controls components for insertion devices.
 
-    .. index:: Ophyd Device; PlanarUndulator
+    Works for: Planar & Revolver
 
     The signals *busy* and *done* convey complementary
     information. *busy* comes from the IOC, while *done* comes
     directly from the controller.
-
-    EXAMPLE::
-
-        undulator = PlanarUndulator("S25ID:USID:", name="undulator")
-
     """
 
-    # X-ray spectrum parameters
-    energy = Component(UndulatorPositioner, "Energy")
-    energy_taper = Component(UndulatorPositioner, "TaperEnergy")
-    gap = Component(UndulatorPositioner, "Gap")
-    gap_taper = Component(UndulatorPositioner, "TaperGap")
-    harmonic_value = Component(EpicsSignal, "HarmonicValueC", kind="config")
-    total_power = Component(EpicsSignalRO, "TotalPowerM.VAL", kind="config")
     # Signals for moving the undulator
     start_button = Component(EpicsSignal, "StartC.VAL", put_complete=True, kind="omitted")
     stop_button = Component(EpicsSignal, "StopC.VAL", kind="omitted")
     busy = Component(EpicsSignalRO, "BusyM.VAL", kind="omitted")
     done = Component(EpicsSignalRO, "BusyDeviceM.VAL", kind="omitted")
     motor_drive_status = Component(EpicsSignalRO, "MotorDriveStatusM.VAL", kind="omitted")
+
+
+class ID_Misc_Mixin(Device):
+    """Common miscellaneous components for insertion devices.
+
+    Works for: Planar & Revolver
+    """
+
     # Miscellaneous control signals
     gap_deadband = Component(EpicsSignal, "DeadbandGapC", kind="config")
     device_limit = Component(EpicsSignal, "DeviceLimitM.VAL", kind="config")
@@ -78,6 +75,52 @@ class PlanarUndulator(Device):
     location = Component(EpicsSignalRO, "LocationM", kind="config")
     version_plc = Component(EpicsSignalRO, "PLCVersionM.VAL", kind="config")
     version_hpmu = Component(EpicsSignalRO, "HPMUVersionM.VAL", kind="config")
+
+
+class ID_Spectrum_Mixin(Device):
+    """Common spectrum components for insertion devices.
+
+    Works for: Planar & Revolver
+    """
+
+    # X-ray spectrum parameters
+    energy = Component(UndulatorPositioner, "Energy")
+    energy_taper = Component(UndulatorPositioner, "TaperEnergy")
+    gap = Component(UndulatorPositioner, "Gap")
+    gap_taper = Component(UndulatorPositioner, "TaperGap")
+    harmonic_value = Component(EpicsSignal, "HarmonicValueC", kind="config")
+    total_power = Component(EpicsSignalRO, "TotalPowerM.VAL", kind="config")
+
+
+class PlanarUndulator(ID_Spectrum_Mixin, ID_Controls_Mixin, ID_Misc_Mixin, Device):
+    """APS Planar Undulator.
+
+    .. index:: Ophyd Device; PlanarUndulator
+
+    APS Use: 34 devices, including 20ID.
+
+    EXAMPLE::
+
+        undulator = PlanarUndulator("S25ID:USID:", name="undulator")
+    """
+
+
+class RevolverInsertionDevice(ID_Spectrum_Mixin, ID_Controls_Mixin, ID_Misc_Mixin, Device):
+    """APS Revolver Insertion Device.
+
+    .. index:: Ophyd Device; PlanarUndulator
+
+    APS Use: Only 08US, 08DS, 34DS.
+
+    EXAMPLE::
+
+        undulator = RevolverInsertionDevice("S08ID:USID:", name="undulator")
+    """
+
+    # Revolver control signals
+    revolver_select = Component(EpicsSignal, "RevolverSelectC.VAL", kind="config")
+    status_position1 = Component(EpicsSignalRO, "Position1M.VAL", kind="config")
+    status_position2 = Component(EpicsSignalRO, "Position2M.VAL", kind="config")
 
 
 # -----------------------------------------------------------------------------
