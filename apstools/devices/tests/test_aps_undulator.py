ab0bf00eb0f767005771c88b1decb2f6c5de681f
Pete R Jemian <prjemian@gmail.com>
2024-07-08 17:40:05 -0500

    TST #975

--- apstools/devices/tests/test_aps_undulator.py
+++ apstools/devices/tests/test_aps_undulator.py
@@ -2,22 +2,41 @@ import pytest
 from ophyd.sim import instantiate_fake_device
 
 from ..aps_undulator import PlanarUndulator
+from ..aps_undulator import Revolver_Undulator
+from ..aps_undulator import STI_Undulator
+from ..aps_undulator import Undulator2M
 
 
-@pytest.fixture()
-def undulator():
-    undulator = instantiate_fake_device(PlanarUndulator, prefix="PSS:255ID:", name="undulator")
-    return undulator
+@pytest.mark.parametrize(
+    "klass, prefix",
+    [
+        [PlanarUndulator, "PSS:255ID:"],
+        [Undulator2M, "S01ID:DSID:"],
+        [Revolver_Undulator, "S08ID:USID:"],
+        [STI_Undulator, "S04ID:USID:"],
+    ],
+)
+def test_set_energy(klass, prefix):
+    undulator = instantiate_fake_device(klass, prefix=prefix, name="undulator")
 
-
-def test_set_energy(undulator):
     assert undulator.start_button.get() == 0
     undulator.energy.set(5)
     assert undulator.energy.setpoint.get() == 5
     assert undulator.start_button.get() == 1
 
 
-def test_stop_energy(undulator):
+@pytest.mark.parametrize(
+    "klass, prefix",
+    [
+        [PlanarUndulator, "PSS:255ID:"],
+        [Undulator2M, "S01ID:DSID:"],
+        [Revolver_Undulator, "S08ID:USID:"],
+        [STI_Undulator, "S04ID:USID:"],
+    ],
+)
+def test_stop_energy(klass, prefix):
+    undulator = instantiate_fake_device(klass, prefix=prefix, name="undulator")
+
     assert undulator.stop_button.get() == 0
     undulator.stop()
     assert undulator.stop_button.get() == 1
