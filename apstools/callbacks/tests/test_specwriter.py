23cf92c79d1a2edd8a403dbfdebd25de72a37596
Pete R Jemian <prjemian@gmail.com>
2025-05-15 16:38:37 -0500

    TST #1059 wip

--- apstools/callbacks/tests/test_specwriter.py
+++ apstools/callbacks/tests/test_specwriter.py
@@ -1,31 +1,63 @@
 from contextlib import nullcontext as does_not_raise
-
+import pathlib
 import pytest
+from bluesky import RunEngine, plans as bp
+from ophyd import SoftPositioner
 
 from ..spec_file_writer import SpecWriterCallback
 from ..spec_file_writer import SpecWriterCallback2
+from ..callback_base import FileWriterCallbackBase
 
 DATA_ISSUE_1083 = """
 #F spec.dat
 #E 1746668725.1978145
 #D Wed May 07 20:45:25 2025
 #C Bluesky  user = prjemian  host = zap
-#O0 
-#o0 
+#O0
+#o0
 #C Wed May 07 20:45:26 2025.  num_events_primary = 0
 #C Wed May 07 20:45:26 2025.  exit_status = fail
 """
 
 
 @pytest.mark.parametrize("callback", [SpecWriterCallback, SpecWriterCallback2])
-def test_filename_defined(callback):
+def test_filename_defined(callback: FileWriterCallbackBase):
     """Test that default file name is not 'None'."""
-    specwriter = callback()
+    specwriter: FileWriterCallbackBase = callback()
     assert specwriter is not None
     assert specwriter.spec_filename is not None
 
 
-def test_issue_1083(tempdir):
+def test_issue_1059(tempdir: pytest.fixture):
+    """
+    AttributeError: 'SpecWriterCallback2' object has no attribute 'data_labels'
+    """
+    data_file: pathlib.Path = tempdir / "issue_1059.dat"
+    sig: SoftPositioner = SoftPositioner(name="sig", init_pos=0)
+    
+    documents: list = []
+    
+    def doc_collector(*args) -> None:
+        nonlocal documents
+        
+        documents.append(args)
+
+    with does_not_raise() as exinfo:
+        specwriter: FileWriterCallbackBase = SpecWriterCallback2()
+        specwriter.newfile(data_file)
+
+        RE: RunEngine = RunEngine()
+        RE.subscribe(specwriter.receiver)
+
+        assert len(documents) == 0
+        num_counts: int = 5
+        RE(bp.count([sig], num=num_counts), doc_collector)
+        assert len(documents) == 3 + num_counts  # start, descriptor, n events, stop
+
+    assert exinfo is None
+
+
+def test_issue_1083(tempdir: pytest.fixture):
     """
     ValueError raised on open data file to append & the file contains no runs.
 
@@ -38,6 +70,6 @@ def test_issue_1083(tempdir):
     assert data_file.exists()
 
     with does_not_raise() as exinfo:
-        specwriter = SpecWriterCallback2()
+        specwriter: FileWriterCallbackBase = SpecWriterCallback2()
         specwriter.newfile(data_file)
     assert exinfo is None
