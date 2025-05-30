57c91d2494f6517ffbf68f2b82b5f3d2ac9b2bc0
Pete R Jemian <prjemian@gmail.com>
2025-05-30 14:36:02 -0500

    MNT #1100 resolve imports, add docstrings, apply isort and other style

--- apstools/devices/acs_motors.py
+++ apstools/devices/acs_motors.py
@@ -1,67 +1,77 @@
 """
-acsMotors provides extra signals that are part of AcsMotionControl motor support
+ACS Motors
+==========
 
-    eoffs   : Encoder offset (doesn't persist through power cycle)
-    e_aoffs : Absolute encoder offset (persists through ACS power cycle)
-    e2offs  : Secondary encoder offset (doesn't persist through power cycle)
-    roffs   : Reference offset
-    stepf   : Step factor (distance per [micro]step)
-    efac    : Encoder factor (distance per encoder count)
-    e2fac   : Secondary encoder factor (distance per encoder count)
-    etype   : Encoder type
-    e2type  : Secondary encoder type
-    homed   : Homing status (using ACS algorithms)
-    
-It also includes (from apstools)
-   - motor resolution fields
-   - "servo" enable/disable (CNEN field) which ACS support uses for steppers as well
-   - the dial readback/setpoint
-    
+:class:`~AcsMotors` provides extra signals that are part of AcsMotionControl motor support.
+
+.. autosummary::
+
+    ~AcsMotion
+    ~AcsMotorMixin
+    ~EpicsMotorWithRes
+    ~EpicsMotorWithResAndCNEN
+    ~EpicsMotorWithResAndCNENAndDial
 """
 
-from ophyd import EpicsSignal, EpicsSignalRO
-from ophyd import EpicsMotor
 from ophyd import Component as Cpt
+from ophyd import EpicsMotor
+from ophyd import EpicsSignalRO
 
-from apstools.devices.mixin_base import DeviceMixinBase
+from .motor_mixins import DeviceMixinBase
+from .motor_mixins import EpicsMotorDialMixin
+from .motor_mixins import EpicsMotorResolutionMixin
+from .motor_mixins import EpicsMotorServoMixin
 
-from apstools.devices import EpicsMotorResolutionMixin
-from apstools.devices import EpicsMotorServoMixin
-from apstools.devices import EpicsMotorDialMixin
 
 class EpicsMotorWithRes(EpicsMotorResolutionMixin, EpicsMotor):
-	pass
+    """Adds support for motor record resolution fields."""
+
 
 class EpicsMotorWithResAndCNEN(EpicsMotorServoMixin, EpicsMotorWithRes):
-	pass 
-	
-class EpicsMotorWithResAndCNENAndDial(EpicsMotorDialMixin, EpicsMotorWithResAndCNEN):
-	pass 
-	
-class AcsMotorMixin(DeviceMixinBase):
+    """
+    Adds "servo" enable/disable (CNEN) field.
 
-    # Encoder offset (doesn't persist through power cycle)
+    ACS support uses for steppers as well.
+    """
+
+
+class EpicsMotorWithResAndCNENAndDial(EpicsMotorDialMixin, EpicsMotorWithResAndCNEN):
+    """Adds support for motor record dial coordinates."""
+
+
+class AcsMotorMixin(DeviceMixinBase):
+    """Components used by EPICS database for ACS Motion Controller."""
+
     eoffs = Cpt(EpicsSignalRO, ":EOFFS", kind="config")
-    # Absolute encoder offset (persists through ACS power cycle)
+    """Encoder offset (doesn't persist through power cycle)"""
+
     e_aoffs = Cpt(EpicsSignalRO, ":E_AOFFS", kind="config")
-    # Secondary encoder offset (doesn't persist through power cycle)
+    """Absolute encoder offset (persists through ACS power cycle)"""
+
     e2offs = Cpt(EpicsSignalRO, ":E2OFFS", kind="config")
-    # Reference offset
+    """Secondary encoder offset (doesn't persist through power cycle)"""
+
     roffs = Cpt(EpicsSignalRO, ":ROFFS", kind="config")
-    # Step factor (distance per [micro]step)
+    """Reference offset"""
+
     stepf = Cpt(EpicsSignalRO, ":STEPF", kind="config")
-    # Encoder factor (distance per encoder count)
+    """Step factor (distance per [micro]step)"""
+
     efac = Cpt(EpicsSignalRO, ":EFAC", kind="config")
-    # Secondary encoder factor (distance per encoder count)
+    """Encoder factor (distance per encoder count)"""
+
     e2fac = Cpt(EpicsSignalRO, ":E2FAC", kind="config")
-    # Encoder type
+    """Secondary encoder factor (distance per encoder count)"""
+
     etype = Cpt(EpicsSignalRO, ":E_TYPE", kind="config")
-    # Secondary encoder type
-    e2type =   Cpt(EpicsSignalRO, ":E2_TYPE", kind="config")
-    # Homing status (using ACS algorithms)
-    homed =   Cpt(EpicsSignalRO, ":HOMED", kind="config")
+    """Encoder type"""
+
+    e2type = Cpt(EpicsSignalRO, ":E2_TYPE", kind="config")
+    """Secondary encoder type"""
+
+    homed = Cpt(EpicsSignalRO, ":HOMED", kind="config")
+    """Homing status (using ACS algorithms)"""
+
 
 class AcsMotor(AcsMotorMixin, EpicsMotorWithResAndCNENAndDial):
-	pass
-
-
+    """AcsMotionControl motor support."""
